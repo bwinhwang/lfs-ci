76eeaff8f9722b8b74ee5baa675e667feb073a19
Bernhard Minks <bernhard@minks.de>
2014-02-11 15:00:58 +0100

    update

--- lib/ci_logging.sh
+++ lib/ci_logging.sh
@@ -41,12 +41,65 @@ message() {
     local type=$1
     local message=$2
 
+    local config=${3-"DATE SPACE TYPE SPACE MESSAGE NEWLINE"}
+
     printf -v date "%-20s" "`date`"
 
-    printf "%s%10s %s\n"  \
-           "${date-}"     \
-           "[${type}]"    \
-           "${message}"
+    for template in ${config}
+    do
+        case "${template}" in 
+            SPACE)   printf " "  ;;
+            NEWLINE) printf "\n" ;;
+            TAB)     printf "\t" ;;
+            DATE)
+                printf "%s" "${date}"
+            ;;
+            TYPE)
+                printf "%10s" "[${type}]"
+            ;;
+            MESSAGE)
+                printf "%s" "${message}"
+            ;;
+            LINE)
+                printf -- "-----------------------------------------------------------------"
+            ;;
+            CALLER)
+                printf "called from Method '%s' in File %s, Line %s"    \
+                    "${FUNCNAME[2]}"                                    \
+                    "${BASH_SOURCE[2]}"                               \
+                    "${BASH_LINENO[1]}"                               
+            ;;
+            STACKTRACE)
+                _stackTrace
+            ;;
+        esac
+
+    done
+
 }
+## @fn      _stackTrace()
+#  @brief   shows the strack trace of this method call
+#  @detail  based on http://blog.yjl.im/2012/01/printing-out-call-stack-in-bash.html
+#           Copyright 2012 Yu-Jie Lin
+#           MIT License
+#           with some layout modification
+#  @param   <none>
+#  @return  <none>
+_stackTrace() {
+    local i=0
+    local FRAMES=${#BASH_LINENO[@]}
+    printf "Strack Trace: \n"
+    # FRAMES-2 skips main, the last one in arrays
+    for ((i=FRAMES-2; i>=0; i--)); do
+        # Grab the source code of the line
+        code=$(sed -n "${BASH_LINENO[i]}{s/^//;p}" "${BASH_SOURCE[i+1]}")
+        printf "File %-30s Line %5d Method %-20s: %-100s\n"   \
+                ${BASH_SOURCE[i+1]}              \
+                ${BASH_LINENO[i]}                \
+                ${FUNCNAME[i+1]}                 \
+                "${code}"
+    done 
+}
+
 
 
--- test/02_logging_debug.sh
+++ test/02_logging_debug.sh
@@ -10,45 +10,65 @@ oneTimeSetUp() {
     date() {
         echo "mocked date command"
     }
+
 }
 
 testLogging() {
 
     assertEquals "simple debug message"                  \
-                 "mocked date command    [debug] foobar" \
+                 "mocked date command     [debug] foobar" \
                  "`debug 'foobar'`" 
 
     local veryLongDebugMessage="this is a very long info message for the logging function in the logging method. this should work without any problem"
     assertEquals "longer debug message"                                   \
-                 "mocked date command    [debug] ${veryLongDebugMessage}" \
+                 "mocked date command     [debug] ${veryLongDebugMessage}" \
                  "`debug \"${veryLongDebugMessage}\"`" 
 
 
     assertEquals "simple info message"                   \
-                 "mocked date command     [info] foobar" \
+                 "mocked date command      [info] foobar" \
                  "`info 'foobar'`" 
 
     assertEquals "longer info message"                                    \
-                 "mocked date command     [info] ${veryLongDebugMessage}" \
+                 "mocked date command      [info] ${veryLongDebugMessage}" \
                  "`info \"${veryLongDebugMessage}\"`" 
 
 
     assertEquals "simple warning message"                \
-                 "mocked date command  [warning] foobar" \
+                 "mocked date command   [warning] foobar" \
                  "`warning 'foobar'`" 
 
     assertEquals "longer warning message"                                 \
-                 "mocked date command  [warning] ${veryLongDebugMessage}" \
+                 "mocked date command   [warning] ${veryLongDebugMessage}" \
                  "`warning \"${veryLongDebugMessage}\"`" 
 
 
     assertEquals "simple error message"                  \
-                 "mocked date command    [error] foobar" \
+                 "mocked date command     [error] foobar" \
                  "`error 'foobar'`" 
 
     assertEquals "longer error message"                                   \
-                 "mocked date command    [error] ${veryLongDebugMessage}" \
+                 "mocked date command     [error] ${veryLongDebugMessage}" \
                  "`error \"${veryLongDebugMessage}\"`" 
+}
+
+testLoggingConfiguration() {
+
+    assertEquals "simple message with configuration"  \
+                 "foobar"                             \
+                 "`message INFO 'foobar' MESSAGE`" 
+
+    assertEquals "simple message with configuration"  \
+                 "mocked date command foobar"         \
+                 "`message INFO 'foobar' 'DATE MESSAGE'`" 
+
+    assertEquals "simple message with configuration"  \
+                 "    [INFO]"                         \
+                 "`message INFO 'foobar' 'TYPE'`" 
+
+    assertEquals "simple message with configuration"  \
+                 "called from Method '_shunit_execSuite' in File lib/shunit2, Line 786" \
+                 "`message INFO 'foobar' 'CALLER'`" 
 }
 
 source lib/shunit2
