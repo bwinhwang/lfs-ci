3417aba5cb06f9eefc060ca6e1586b51f4cf0bf7
Bernhard Minks <bernhard.minks.ext@nsn.com>
2014-02-17 12:19:32 +0100

    add jenkins startup

---
+++ bin/startJenkins.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ROOT=/var/fpwork/demx2fk3/ci/
+JENKINS_WAR=${ROOT}/lib/java/jenkins/jenkins-1.532.1.war
+JENKINS_HOME=${ROOT}/home/
+JENKINS_ROOT=${ROOT}/root/
+JENKINS_ARGS=" --httpPort=1234
+-httpListenAddress=
+"
+
+export JENKINS_HOME JENKINS_ROOT
+
+
+mkdir -p ${JENKINS_HOME} ${JENKINS_ROOT} ${ROOT}/log
+ulimit -u 40960
+
+cd ${JENKINS_HOME}
+/usr/bin/java -jar ${JENKINS_WAR} \
+        --httpsPort=12443 \
+        --httpsCertificate=${ROOT}/etc/lfs-ci.emea.nsn-net.net.crt \
+        --httpsPrivateKey=${ROOT}/etc/lfs-ci.emea.nsn-net.net.key  \
+        --httpPort=1280 \
+        --ajp13Port=12800 \
+        > ${ROOT}/log/jenkins.log 2>&1 
+
+
+
+
+
+
+
