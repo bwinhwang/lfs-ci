3bcc90c76eb682178e5893abc01e98cabe4c6966
Bernhard Minks <bernhard.minks.ext@nsn.com>
2014-04-07 13:54:08 +0200

    fix custom scm stuff

--- lib/customScm.sh
+++ lib/customScm.sh
@@ -78,6 +78,8 @@ getJobNameFromUrl() {
 #  @return  1 if if a build is not required, 0 otherwise
 actionCompare() {
 
+    exit 1
+
     if [[ -z "${REVISION_STATE_FILE}" ]] ; then
         info "no old revision state file found"
         exit 0
@@ -187,8 +189,8 @@ actionCalculate() {
     newRevisionsFile=$(createTempFile)
     # _createRevisionsTxtFile ${newRevisionsFile}
 
-    execute rsync -ae ssh ${newRevisionsFile} \
-                ${jenkinsMasterServerPath}/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/revisions.txt
+    # execute rsync -ae ssh ${newRevisionsFile} \
+    #             ${jenkinsMasterServerPath}/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/revisions.txt
 
     return 
 }
