4a23684ef616a54df2d15747ea486745852647fa
Bernhard Minks <bernhard@minks.de>
2014-04-07 13:56:04 +0200

    wip

--- lib/customScm.sh
+++ lib/customScm.sh
@@ -1,70 +1,5 @@
 #!/bin/bash
 
-## @fn      dumpCustomScmEnvironmentVariables(  )
-#  @brief   dump / show all environment variables which are related to Custom SCM Jenkins Plugin
-#  @param   <none>
-#  @return  <none>
-dumpCustomScmEnvironmentVariables() {
-    for var in BUILD_DIR               \
-               BUILD_NUMBER            \
-               BUILD_URL               \
-               BUILD_URL_LAST          \
-               BUILD_URL_LAST_STABLE   \
-               BUILD_URL_LAST_SUCCESS  \
-               CHANGELOG               \
-               JENKINS_HOME            \
-               JENKINS_URL             \
-               JOB_DIR                 \
-               JOB_NAME                \
-               OLD_REVISION_STATE_FILE \
-               REVISION_STATE_FILE     \
-               UPSTREAM_BUILD          \
-               UPSTREAM_JOB_URLS       \
-               UPSTREAM_PROJECT        \
-               WORKSPACE 
-    do
-        debug "$(printf "%30s %-30s\n" "${var}" "${!var}")"
-    done
-
-    return
-}
-
-## @fn      createPropertiesFileForBuild()
-#  @brief   create a properties file, which are used by the build script.
-#  @details format is bash. so you can source the file
-#  @param   <none>
-#  @return  <none>
-createPropertiesFileForBuild() {
-    execute rm -rf ${WORKSPACE}/.properties
-    echo UPSTREAM_BUILD=${UPSTREAM_BUILD}     >  ${WORKSPACE}/.properties
-    echo UPSTREAM_PROJECT=${UPSTREAM_PROJECT} >> ${WORKSPACE}/.properties
-    return
-}
-
-## @fn      getBuildNumberFromUrl( $url  )
-#  @brief   get the build number out of a jenkins url
-#  @details the format is
-#           http://maxi.emea.nsn-net.net:1280/job/custom_SCM_test_-_down/634/
-#  @param   {url}    a jenkins url
-#  @return  build number
-getBuildNumberFromUrl() {
-    local url=$1
-    cut -d/ -f 6 <<< ${url}
-    return
-}
-
-## @fn      getJobNameFromUrl( $url  )
-#  @brief   get the job name out of a jenkins url
-#  @details the format is
-#           http://maxi.emea.nsn-net.net:1280/job/custom_SCM_test_-_down/634/
-#  @param   {url}    a jenkins url
-#  @return  build number
-getJobNameFromUrl() {
-    local url=$1
-    cut -d/ -f 5 <<< ${url}
-    return
-}
-
 ## @fn      actionCompare()
 #  @brief   this command is called by jenkins custom scm plugin via a
 #           polling trigger. It should decide, if a build is required
@@ -169,8 +104,8 @@ _createRevisionsTxtFile() {
 }
 
 ## @fn      actionCheckout()
-#  @brief   action which is called by custom scm jenkins plugin to create or update a workspace
-#  @details 
+#  @brief   action which is called by custom scm jenkins plugin to create or update a workspace and create the changelog
+#  @details the create workspace task is empty here. We just calculate the changelog
 #  @param   <none>
 #  @return  <none>
 actionCheckout() {
