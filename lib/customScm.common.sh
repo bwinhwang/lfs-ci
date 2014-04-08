3285c7210d832789da81aa737c0d45d1dbec9939
Bernhard Minks <bernhard@minks.de>
2014-04-08 10:45:23 +0200

    testing custom scm scripting

--- lib/customScm.common.sh
+++ lib/customScm.common.sh
@@ -23,7 +23,7 @@ dumpCustomScmEnvironmentVariables() {
                UPSTREAM_PROJECT        \
                WORKSPACE 
     do
-        debug "$(printf "%20s %-30s\n" "${var}" "${!var}")"
+        debug "$(printf "%25s %-30s\n" "${var}" "${!var}")"
     done
 
     return
