96e111c009d367785e18d929e595b1e19789743a
Bernhard Minks <bernhard@minks.de>
2014-04-08 11:37:32 +0200

    testing custom scm scripting

--- lib/customScm.svn.sh
+++ lib/customScm.svn.sh
@@ -38,31 +38,9 @@ actionCompare() {
         exit 0
     fi
 
-    # getting old revision file 
-    oldRevisionsFile=$(createTempFile)
-
-    local oldProjectName=$(getJobNameFromUrl     ${BUILD_URL_LAST})
-    local oldBuildNumber=$(getBuildNumberFromUrl ${BUILD_URL_LAST})
-
-    info "last project was ${oldProjectName} / ${oldBuildNumber}"
-
-    if [[ -z "${oldBuildNumber}" ]] ; then
-        info "never build up to now, trigger new build"
-        exit 0
-    fi
-
-    local oldRevisionsFileOnServer=${jenkinsMasterServerPath}/jobs/${oldProjectName}/builds/${oldBuildNumber}/revisions.txt
-
-    if ! runOnMaster test -e ${oldRevisionsFileOnServer} ; then
-        info "revisions file does not exist, trigger new build"
-        exit 0
-    fi
-
-    # TODO: demx2fk3 2014-04-08 use long version of parameters
-    execute rsync -ae ssh ${jenkinsMasterServerHostName}:${oldRevisionsFileOnServer} \
-                          ${oldRevisionsFile}
-
     # generate the new revsions file
+    local oldRevisionsFile=${REVISION_STATE_FILE}
+
     local newRevisionsFile=$(createTempFile)
     _createRevisionsTxtFile ${newRevisionsFile}
 
@@ -157,17 +135,8 @@ actionCheckout() {
 actionCalculate() {
 
     debug "generate the new revision file"
-    newRevisionsFile=$(createTempFile)
+    newRevisionsFile=${REVISION_STATE_FILE}
     _createRevisionsTxtFile ${newRevisionsFile}
-
-    # maybe we are to fast. just wait a second...
-    if runOnMaster test ! -d ${jenkinsMasterServerPath}/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/ ; then
-        sleep 1
-    fi
-
-    debug "copy new revision file to jenkins master server"
-    execute rsync -ae ssh ${newRevisionsFile} \
-                    ${jenkinsMasterServerHostName}:${jenkinsMasterServerPath}/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/revisions.txt
 
     return 
 }
