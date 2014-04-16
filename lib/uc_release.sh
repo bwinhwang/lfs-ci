07ee527fbbe54b99bff4e62e7099422f9c7bd108
Bernhard Minks <bernhard@minks.de>
2014-04-16 15:45:40 +0200

    update

--- lib/uc_release.sh
+++ lib/uc_release.sh
@@ -2,13 +2,13 @@
 
 ci_job_release() {
 
-    requiredParameters JOB_NAME BUILD_NUMBER
+    requiredParameters UPSTREAM_PROJECT UPSTREAM_BUILD
 
     # setBuildDescription "${JOB_NAME}" "${BUILD_NUMBER}" "trunk-${BUILD_NUMBER}"
 
     # TODO: demx2fk3 2014-04-16 we need the svn revision "somehow"
 
-    ${LFS_CI_ROOT}/bin/getUpStreamProject -j ${JOB_NAME} -b ${BUILD_NUMBER} -h ${jenkinsMasterServerPath}
+    ${LFS_CI_ROOT}/bin/getUpStreamProject -j ${UPSTREAM_PROJECT} -b ${UPSTREAM_BUILD} -h ${jenkinsMasterServerPath}
 
     error "not implemented"
 
