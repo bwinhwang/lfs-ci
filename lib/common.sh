e4d27a803b19ef7493bb4054a875708e3f37263f
Bernhard Minks <bernhard@minks.de>
2014-05-15 14:34:33 +0200

    add kernel 3 branch

--- lib/common.sh
+++ lib/common.sh
@@ -71,6 +71,10 @@ getLocationName() {
 
     # 2014-02-17 demx2fk3 TODO do this in a better wa
     case ${location} in
+        kernel3x)
+            trace "TODO: mapping location name from kernel3x to KERNEL_3.x_DEV"
+            echo KERNEL_3.x_DEV
+        ;;
         trunk)
             trace "TODO: mapping location name from trunk to pronb-developer"
             echo pronb-developer
