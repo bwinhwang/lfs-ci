e1a47a1ac691488c32bef339ef494664292a28cb
Bernhard Minks <bernhard@minks.de>
2014-05-22 14:55:33 +0200

    fix ?

--- lib/config.sh
+++ lib/config.sh
@@ -38,9 +38,9 @@ declare -a branchToLocationMap=( ["trunk"]="pronb-developer" \
                                )
 
 # define the mapping from branch to label/tag name
-declare -A branchToTagRegexMap=( ["pronb-developer"]="BM_PS_LFS_OS_$(date +%Y)_$(date +%m)_([0-9][0-9])" \
-                                          ["FB1404"]="BM_FB_PS_LFS_OS_1404_04_([0-9][0-9])" \
-                                  ["KERNEL_3.x_DEV"]="BM_KERNEL3x_PS_LFS_OS_$(date +%Y)_$(date +%m)_([0-9][0-9])" \
+declare -A branchToTagRegexMap=( ["pronb-developer"]="PS_LFS_OS_$(date +%Y)_$(date +%m)_([0-9][0-9])" \
+                                          ["FB1404"]="FB_PS_LFS_OS_1404_04_([0-9][0-9])" \
+                                  ["KERNEL_3.x_DEV"]="KERNEL3x_PS_LFS_OS_$(date +%Y)_$(date +%m)_([0-9][0-9])" \
                                )
 
 ## @fn      getConfig( key )
