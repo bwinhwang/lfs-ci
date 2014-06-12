9429c461660cb4b343e6d2b09b51d2c54ff4421f
Bernhard Minks <bernhard.minks.ext@nsn.com>
2014-06-12 15:46:46 +0200

    label fix?

--- bin/LfsScmTools.pl
+++ bin/LfsScmTools.pl
@@ -927,7 +927,7 @@ SOURCES:
 
                 }
                 printf "%s-%s: %s\n", $source->{directory}, $platform, join( " ", @filteredDeps );
-                printf "\t/usr/bin/time -v build -L \$@.log -C %s %s --label=\$\{LABEL\}\n\n", $source->{directory}, $platform;
+                printf "\t/usr/bin/time -v build -L \$@.log -C %s %s --label=\$(LABEL)\n\n", $source->{directory}, $platform;
             }
         } elsif( $self->{style} eq "legacy" ) {
             printf "%s %s - %s\n",
