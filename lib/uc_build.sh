4379b84973ccf75faf08344e9fad5ad42fb75b81
Bernhard Minks <bernhard.minks.ext@nsn.com>
2014-06-25 19:47:42 +0200

    fix

--- lib/uc_build.sh
+++ lib/uc_build.sh
@@ -255,7 +255,7 @@ _createWorkspace() {
         local master=$(getConfig jenkinsMasterServerHostName)
         mustHaveValue "${dir}" "build directory on master"
         info "copy revision state file from master"
-        execute rsync -avP ssh ${master}:${dir}/revisionstate.xml ${WORKSPACE}/revisions.txt
+        execute rsync -avPe ssh ${master}:${dir}/revisionstate.xml ${WORKSPACE}/revisions.txt
         rawDebug ${WORKSPACE}/revisions.txt
     fi
 
