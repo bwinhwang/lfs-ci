adf5334b8a3c7483ee9d18d4d509a3826904ef9b
Bernhard Minks <bernhard.minks.ext@nsn.com>
2014-07-03 14:19:33 +0200

    fix

--- lib/customSCM.filelisting.sh
+++ lib/customSCM.filelisting.sh
@@ -13,7 +13,7 @@ actionCompare() {
     fi
 
     export subTaskName=$(getSubTaskNameFromJobName)
-    local directoryNameToSynchronize=$(getConfig directoryNameToSynchronize)
+    local directoryNameToSynchronize=$(getConfig ADMIN_sync_share_directoryNameToSynchronize)
 
     # read the revision state file
     # format:
@@ -52,7 +52,7 @@ actionCheckout() {
     cat < /dev/null > "${CHANGELOG}"
 
     export subTaskName=$(getSubTaskNameFromJobName)
-    local directoryNameToSynchronize=$(getConfig directoryNameToSynchronize)
+    local directoryNameToSynchronize=$(getConfig ADMIN_sync_share_directoryNameToSynchronize)
     local oldFileListing=$(createTempFile)
     local newFileListing=$(createTempFile)
     local checksum=$(ls -lat ${directoryNameToSynchronize} | md5sum | cut -d" " -f 1)
