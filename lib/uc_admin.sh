ea5d2bad8deebeda7c93a07299301e6820969b9d
Bernhard Minks <bernhard.minks.ext@nsn.com>
2014-07-03 14:42:50 +0200

    fix

--- lib/uc_admin.sh
+++ lib/uc_admin.sh
@@ -36,12 +36,12 @@ synchronizeShare() {
 
     execute ssh ${remoteServer} mkdir -p ${remotePath}
 
-    info "synchronize ${localPath} to  ${remoteServer}:${remotePath}"
+    info "synchronize ${localPath} to ${remoteServer}:${remotePath}"
     # TODO: demx2fk3 2014-07-03 add an execute
     for entry in $(${LFS_CI_ROOT}/bin/xpath -q -e '/log/logentry/paths/path/node()' ${WORKSPACE}/changelog.xml )
     do
         info "transferting ${entry} to ${remoteServer}:${remotePath}"
-        rsync -avrPe ssh ${entry} ${remoteServer}:${remotePath}
+        rsync -ave ssh ${entry} ${remoteServer}:${remotePath}
     done
 
     return
