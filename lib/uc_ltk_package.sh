b9c2fecbe154a24216a9f803a993b258527a3202
Bernhard Minks <bernhard@minks.de>
2014-11-05 12:58:52 +0100

    add support for ltk

---
+++ lib/uc_ltk_package.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+[[ -z ${LFS_CI_SOURCE_artifacts} ]] && source ${LFS_CI_ROOT}/lib/artifacts.sh
+[[ -z ${LFS_CI_SOURCE_package}   ]] && source ${LFS_CI_ROOT}/lib/package.sh
+
+## @fn      ci_job_package()
+#  @brief   create a package from the build results for the testing / release process
+#  @details copy all the artifacts from the sub jobs into the workspace and create the release structure
+#  @param   <none>
+#  @return  <none>
+ci_job_package() {
+    info "package the build results"
+
+    local workspace=$(getWorkspaceName)
+    mustHaveWorkspaceName
+    mustHaveCleanWorkspace
+    mustHaveWritableWorkspace
+
+    debug "workspace is ${workspace}"
+
+    copyArtifactsToWorkspace ${UPSTREAM_PROJECT} ${UPSTREAM_BUILD}
+
+    mustHaveNextCiLabelName
+    local label=$(getNextCiLabelName)
+    setBuildDescription "${JOB_NAME}" "${BUILD_NUMBER}" "${label}"
+    info "label name is ${label}"
+
+    local localDirectory=${workspace}/upload
+    execute mkdir -p ${localDirectory}
+
+#     find ${workspace}
+# 
+#     for bldDirectory in ${workspace}/bld/bld-*brm*-* ; do
+#         info "bldDirectory is ${bldDirectory}"
+#         [[ -d ${bldDirectory} ]] || continue
+#         local baseNameBldDir=$(basename ${bldDirectory})
+# 
+#         info "copy uboot for ${bldDirectory}..."
+# 
+#         local srcDirectory=${bldDirectory}
+#         local dstDirectory=${workspace}/upload/bld/${baseNameBldDir}
+# 
+#         execute mkdir -p ${dstDirectory}
+#         execute rsync -av --delete ${srcDirectory}/. ${dstDirectory}/
+# 
+#     done
+# 
+#     copyReleaseCandidateToShare
+
+    return 0
+}
+
