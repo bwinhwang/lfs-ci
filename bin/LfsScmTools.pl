173601a75559719310fc587ac8a840a8245bd1dc
Bernhard Minks <bernhard@minks.de>
2014-11-06 11:06:20 +0100

    add unit test for getReleaseNoteContent, fix for reading file

--- bin/LfsScmTools.pl
+++ bin/LfsScmTools.pl
@@ -2899,7 +2899,6 @@ sub config {
     if( not $obj->{config}{handler} ) {
         DEBUG "creating config handler";
         $obj->{config}{handler} = Config->new();
-        # $obj->{config}{handler}->loadData();
     }
     return $obj->{config}{handler};
 }
