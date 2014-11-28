5c99eda1c9f309c8b8459e9f75c29dfd130170ce
Bernhard Minks <bernhard@minks.de>
2014-11-28 20:48:08 +0100

    wip: update ut

--- test/14_uc_klocwork.sh
+++ test/14_uc_klocwork.sh
@@ -29,9 +29,20 @@ oneTimeSetUp() {
     getConfig() {
         mockedCommand "getConfig $@"
         case $1 in
-            LFS_CI_uc_klocwork_project_name)
-                echo PS_LFS_DDAL
-                ;;
+            LFS_CI_uc_klocwork_project_name)         echo PS_LFS_DDAL ;;
+            LFS_CI_uc_klocwork_port)                 echo 12345 ;;
+            LFS_CI_uc_klocwork_hostname)             echo klocwork_hostname ;;
+            LFS_CI_uc_klocwork_licence_port)         echo 54321 ;;
+            LFS_CI_uc_klocwork_licence_host)         echo licence_hostname ;;
+            LFS_CI_uc_klocwork_cmd_kwinject)         echo /path/to/bin/kwinject ;;
+            LFS_CI_uc_klocwork_cmd_kwadmin)          echo /path/to/bin/kwadmin ;;
+            LFS_CI_uc_klocwork_cmd_kwbuildproject)   echo /path/to/bin/kwbuildproject ;;
+            LFS_CI_uc_klocwork_url)                  echo http://klocwork_hostname:12345/ ;;
+            LFS_CI_uc_klocwork_template)             echo kw_template ;;
+            LFS_CI_uc_klocwork_tables)               echo kw_tables ;;
+            LFS_CI_uc_klocwork_architectures)        echo arm powerpc mips i386 x64 ;;
+            LFS_CI_uc_klocwork_report_python_script) echo http://svn/path/to/script.py ;;
+            LFS_CI_uc_klocwork_python_home)          echo /path/to/python/home ;;
         esac
     }
 
