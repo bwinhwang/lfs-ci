68396ce3b958e9e1fac033695ad2e14da8aa8c6d
Name=Ambrosch Erwin <eambrosc@ulegcpeag12.emea.nsn-net.net>
2014-12-02 12:10:13 +0100

    Added test case testFail()

--- test/13_setRevisionAsJobDescription.sh
+++ test/13_setRevisionAsJobDescription.sh
@@ -7,9 +7,6 @@ initTempDirectory
 
 oneTimeSetUp() {
   cp test/data/13_setRevisionAsJobDescription.xml test/data/13_setRevisionAsJobDescription.xml.bak
-  svnLog() {
-      cat test/data/13_setRevisionAsJobDescription.svn.xml
-  }
 }
 
 oneTimeTearDown() {
@@ -17,11 +14,19 @@ oneTimeTearDown() {
 }
 
 testDescription() {
+  svnLog() {
+      cat test/data/13_setRevisionAsJobDescription.svn.xml
+  }
   setRevisionAsJobDescription test/data/13_setRevisionAsJobDescription.xml BranchName
   assertEquals "Number of description is not 1." 1 $(grep 114389 test/data/13_setRevisionAsJobDescription.xml | wc -l)
 }
 
-#oneTimeSetUp
-#oneTimeTearDown
-#testDescription
+testFail() {
+  svnLog() {
+      cat test/data/13_setRevisionAsJobDescription_wrong.svn.xml
+  }
+  setRevisionAsJobDescription test/data/13_setRevisionAsJobDescription.xml BranchName
+  assertEquals "Number of description is not 0." 0 $(grep 114389 test/data/13_setRevisionAsJobDescription.xml | wc -l)
+}
+
 . lib/shunit2
