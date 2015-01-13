2399d27df4cf9cf54fed6991f622513c4ba5f6e6
Bernhard Minks <bernhard.minks.ext@nsn.com>
2015-01-13 17:25:24 +0100

    fix

--- lib/uc_test.sh
+++ lib/uc_test.sh
@@ -20,7 +20,7 @@ ci_job_test() {
     local upstreamBuildNumber=${UPSTREAM_BUILD}
     info "upstreamProject data: ${upstreamProject} / ${upstreamBuildNumber}"
 
-    local buildJobName=$(getBuildBuildNumberFromUpstreamProject ${UPSTREAM_PROJECT} ${UPSTREAM_BUILD})
+    local buildJobName=$(getBuildJobNameFromUpstreamProject ${UPSTREAM_PROJECT} ${UPSTREAM_BUILD})
     local buildBuildNumber=$(getBuildBuildNumberFromUpstreamProject ${UPSTREAM_PROJECT} ${UPSTREAM_BUILD})
 
     local requiredArtifacts=$(getConfig LFS_CI_UC_test_required_artifacts)
