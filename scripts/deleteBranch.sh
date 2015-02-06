ee7674601820507a47c892982066f97a5df6529b
Erwin Ambrosch <erwin.ambrosch.ext@nsn.com>
2015-02-06 13:00:03 +0100

    Implemented additional requirements for delete branch and added tests.

--- scripts/deleteBranch.sh
+++ scripts/deleteBranch.sh
@@ -14,6 +14,7 @@ info "# DELETE_SHARE:     $DELETE_SHARE"
 info "# LRC_MOVE_SVN:     $LRC_MOVE_SVN"
 info "# LRC_DELETE_JOBS:  $LRC_DELETE_JOBS"
 info "# LRC_DELETE_SHARE: $DELETE_SHARE"
+info "# DEBUG:            $DEBUG"
 info "# COMMENT:          $COMMENT"
 info "###############################################################"
 
@@ -21,11 +22,48 @@ SVN_REPO=$(getConfig lfsSourceRepos)
 SVN_DIR="os"
 SVN_BLD_DIR="trunk/bldtools"
 SHARE="/build/home/CI_LFS/Release_Candidates"
-BRANCH_TYPE=$(echo $BRANCH  | cut -c1,2)
-YY=$(echo $BRANCH  | cut -c3,4)
-MM=$(echo $BRANCH  | cut -c5,6)
-YYYY=$((2000+YY))
+BLD_SHARE="/build/home/SC_LFS/releases/bld"
+PKG_SHARE="/build/home/SC_LFS/pkgpool"
 
+
+#######################################################################
+#
+# HELPER FUNCTIONS - maybe move them to common.sh
+#
+#######################################################################
+
+
+## @fn      getBranchPart()
+#  @brief   provides the parts of a branch name
+#  @param   <branch> the name of a branch eg FB1408
+#  @param   <what> the part to get. Can be YY | YYYY | MM | NR | TYPE
+#           NR is only valid for MD branches.
+#  @return  <none>
+getBranchPart() {
+    local branch=$1
+    local what=$2
+    local branch_type=$(echo ${branch} | cut -c1,2)
+
+    if [ "${branch_type}" == "FB" ]; then
+        local yy=$(echo ${branch}  | cut -c3,4)
+        local mm=$(echo ${branch}  | cut -c5,6)
+        local yyyy=$((2000+yy))
+    elif [ "${branch_type}" == "MD" ]; then
+        local yy=$(echo ${branch}  | cut -c4,5)
+        local nr=$(echo ${branch}  | cut -c3)
+        local mm=$(echo ${branch}  | cut -c6,7)
+        local yyyy=$((2000+yy))
+    else
+        error "Only FB and MD branches are supported."
+        return 1
+    fi
+
+    [[ ${what} == YY ]] && echo ${yy}
+    [[ ${what} == YYYY ]] && echo ${yyyy}
+    [[ ${what} == MM ]] && echo ${mm}
+    [[ ${what} == TYPE ]] && echo ${branch_type}
+    [[ ${what} == NR ]] && echo ${nr}
+}
 
 ## @fn      getEclValue()
 #  @brief   get value from ecl for key
@@ -36,10 +74,41 @@ getEclValue() {
     local key=$1
     local branch=$2
     local svnEclRepo=$(echo ${SVN_REPO} | awk -F/ '{print $1"//"$2$3}')
-    #local value=$(svn cat ${SVN_CREDS} ${svnEclRepo}/isource/svnroot/BTS_SCM_PS/ECL/${branch}/ECL_BASE/ECL | grep ${key} | cut -d'=' -f2)
     local value=$(svn cat --username eambrosc --password jesusih --non-interactive --trust-server-cert ${svnEclRepo}/isource/svnroot/BTS_SCM_PS/ECL/${branch}/ECL_BASE/ECL | grep ${key} | cut -d'=' -f2)
     echo ${value}
 }
+
+
+#######################################################################
+#
+# HELPER FUNCTIONS
+#
+#######################################################################
+
+
+__checkParams() {
+    [[ ! "$BRANCH" ]] && { error "BRANCH must be specified"; return 1; }
+    echo $BRANCH | grep -e "^FB[0-9]\{4\}\|^MD[0-9]\{4\}\|TEST_ERWIN" || { error "$BRANCH is not valid."; return 1; }
+}
+
+__cmd() {
+    if [ $DEBUG == true ]; then
+        debug $@
+        echo [DEBUG] $@
+    else
+        info runnig command: $@
+        echo runnig command: $@
+        #eval $@
+    fi
+}
+
+
+#######################################################################
+#
+# WORKING FUNCTIONS
+#
+#######################################################################
+
 
 ## @fn      moveBranchSvn()
 #  @brief   move $BRANCH in svn
@@ -47,16 +116,17 @@ getEclValue() {
 #  @return  <none>
 moveBranchSvn() {
     svn ls ${SVN_CREDS} ${SVN_REPO}/${SVN_DIR}/${BRANCH} && {
-        echo svn move -m "moved ${BRANCH} to obsolete" ${SVN_REPO}/${SVN_DIR}/${BRANCH} ${SVN_REPO}/${SVN_DIR}/obsolete;
+        __cmd svn move -m \"moved ${BRANCH} to obsolete\" \
+            ${SVN_REPO}/${SVN_DIR}/${BRANCH} ${SVN_REPO}/${SVN_DIR}/obsolete;
     }
 
     svn ls ${SVN_CREDS} ${SVN_REPO}/${SVN_DIR}/${SVN_BLD_DIR}/locations-${BRANCH} && {
-        echo svn move -m "moved locations-${BRANCH} to obsolete" \
+        __cmd svn move -m \"moved locations-${BRANCH} to obsolete\" \
             ${SVN_REPO}/${SVN_DIR}/${SVN_BLD_DIR}/locations-${BRANCH} ${SVN_REPO}/${SVN_DIR}/${SVN_BLD_DIR}/obsolete;
     }
 
     svn ls ${SVN_CREDS} ${SVN_REPO}/${SVN_DIR}/${SVN_BLD_DIR}/locations-${BRANCH}_FSMR4 && {
-        echo svn move -m "moved locations-${BRANCH}_FSMR4 to obsolete" \
+        __cmd svn move -m \"moved locations-${BRANCH}_FSMR4 to obsolete\" \
             ${SVN_REPO}/${SVN_DIR}/${SVN_BLD_DIR}/locations-${BRANCH}_FSMR4 ${SVN_REPO}/${SVN_DIR}/${SVN_BLD_DIR}/obsolete;
     }
     return 0
@@ -68,7 +138,7 @@ moveBranchSvn() {
 #  @return  <none>
 LRC_moveBranchSvn() {
     svn ls ${SVN_CREDS} ${SVN_REPO}/${SVN_DIR}/${SVN_BLD_DIR}/locations-LRC_${BRANCH} && {
-        echo svn move -m "moved locations-LRC_${BRANCH} to obsolete" \
+        __cmd svn move -m \"moved locations-LRC_${BRANCH} to obsolete\" \
             ${SVN_REPO}/${SVN_DIR}/${SVN_BLD_DIR}/locations-LRC_${BRANCH} ${SVN_REPO}/${SVN_DIR}/${SVN_BLD_DIR}/obsolete;
     }
     return 0
@@ -79,15 +149,56 @@ LRC_moveBranchSvn() {
 #  @param   <none>
 #  @return  <none>
 deleteBranchShare() {
+    local branchType=$(getBranchPart ${BRANCH} TYPE)
+    local mm=$(getBranchPart ${BRANCH} MM)
+    local yyyy=$(getBranchPart ${BRANCH} YYYY)
     local keepRelease=$(getEclValue "ECL_PS_LFS_OS" ${BRANCH})
-    local dirPattern="${BRANCH_TYPE}_PS_LFS_OS_${YYYY}_${MM}*"
+    local dirPattern="${branchType}_PS_LFS_OS_${yyyy}_${mm}*"
     local dirsToDelete=$(find ${SHARE} -maxdepth 2 -type d -name "${dirPattern}" | grep -v ${keepRelease})
 
+    info "CLEAN $SHARE"
     info "keep release: ${keepRelease}"
     for DIR in $dirsToDelete
     do
-        echo rm -rf $DIR
-        info "Deleted $DIR"
+        __cmd rm -rf $DIR
+    done
+}
+
+## @fn      deleteBranchBldShare()
+#  @brief   delete data for BRANCH on bld share
+#  @param   <none>
+#  @return  <none>
+deleteBranchBldShare() {
+    local branchType=$(getBranchPart ${BRANCH} TYPE)
+    local mm=$(getBranchPart ${BRANCH} MM)
+    local yyyy=$(getBranchPart ${BRANCH} YYYY)
+    local keepRelease=$(getEclValue "ECL_PS_LFS_OS" ${BRANCH})
+    local dirPattern="${branchType}_PS_LFS_OS_${yyyy}_${mm}*"
+    local dirsToDelete=$(find ${BLD_SHARE} -maxdepth 2 -type d -name "${dirPattern}" | grep -v ${keepRelease})
+
+    info "CLEAN $BLD_SHARE"
+    info "keep release: ${keepRelease}"
+    for DIR in $dirsToDelete
+    do
+        __cmd rm -rf $DIR
+    done
+}
+
+## @fn      deleteBranchPkgShare()
+#  @brief   delete data for BRANCH on pkgpool share
+#  @param   <none>
+#  @return  <none>
+deleteBranchPkgShare() {
+    local branchType=$(getBranchPart ${BRANCH} TYPE)
+    local mm=$(getBranchPart ${BRANCH} MM)
+    local yyyy=$(getBranchPart ${BRANCH} YYYY)
+    local dirPattern="${branchType}_PS_LFS_PKG_${yyyy}_${mm}*"
+    local dirsToDelete=$(find ${PKG_SHARE} -maxdepth 1 -type d -name "${dirPattern}")
+
+    info "CLEAN $PKG_SHARE"
+    for DIR in $dirsToDelete
+    do
+        __cmd rm -rf $DIR
     done
 }
 
@@ -96,28 +207,46 @@ deleteBranchShare() {
 #  @param   <none>
 #  @return  <none>
 LRC_deleteBranchShare() {
+    local branchType=$(getBranchPart ${BRANCH} TYPE)
+    local mm=$(getBranchPart ${BRANCH} MM)
+    local yyyy=$(getBranchPart ${BRANCH} YYYY)
     local keepRelease=$(getEclValue "ECL_PS_LRC_LCP_LFS_OS" ${BRANCH})
-    local dirPattern="${BRANCH_TYPE}_LRC_LCP_PS_LFS_OS_${YYYY}_${MM}*"
+    local dirPattern="${branchType}_LRC_LCP_PS_LFS_OS_${yyyy}_${mm}*"
     local dirsToDelete=$(find ${SHARE} -maxdepth 2 -type d -name "${dirPattern}" | grep -v ${keepRelease})
 
+    info "CLEAN $SHARE LRC"
     info "keep LRC release: ${keepRelease}"
     for DIR in $dirsToDelete
     do
-        echo rm -rf $DIR
-        info "Deleted $DIR"
+        __cmd rm -rf $DIR
     done
 }
 
-__checkParams() {
-    [[ ! "$BRANCH" ]] && { error "BRANCH must be specified"; return 1; }
-    echo $BRANCH | grep -e "^FB[0-9]\{4\}\|^MD[0-9]\{4\}" || { error "$BRANCH is not valid."; return 1; }
+## @fn      LRC_deleteBranchBldShare()
+#  @brief   delete date for BRANCH on bld share for LRC
+#  @param   <none>
+#  @return  <none>
+LRC_deleteBranchBldShare() {
+    local branchType=$(getBranchPart ${BRANCH} TYPE)
+    local mm=$(getBranchPart ${BRANCH} MM)
+    local yyyy=$(getBranchPart ${BRANCH} YYYY)
+    local keepRelease=$(getEclValue "ECL_PS_LRC_LCP_LFS_OS" ${BRANCH})
+    local dirPattern="${branchType}_LRC_LCP_PS_LFS_OS_${yyyy}_${mm}*"
+    local dirsToDelete=$(find ${BLD_SHARE} -maxdepth 2 -type d -name "${dirPattern}" | grep -v ${keepRelease})
+
+    info "CLEAN $BLD_SHARE LRC"
+    info "keep LRC release: ${keepRelease}"
+    for DIR in $dirsToDelete
+    do
+        __cmd rm -rf $DIR
+    done
 }
 
 __checkParams || { error "Params check failed."; exit 1; }
 
-[[ ${MOVE_SVN} == true ]] && moveBranchSvn || info "Not moving $BRANCH in SVN"
-[[ ${DELETE_SHARE} == true ]] && deleteBranchShare || info "Not deleting $BRANCH on share"
+[[ ${MOVE_SVN} == true ]] && moveBranchSvn || info "Not moving $BRANCH in repo"
+[[ ${DELETE_SHARE} == true ]] && { deleteBranchShare; deleteBranchBldShare; deleteBranchPkgShare; } || info "Not deleting $BRANCH on share"
 
-[[ ${LRC_MOVE_SVN} == true ]] && LRC_moveBranchSvn || info "Not moving $BRANCH in SVN for LRC"
-[[ ${LRC_DELETE_SHARE} == true ]] && LRC_deleteBranchShare || info "Not deleting $BRANCH on share for LRC"
+[[ ${LRC_MOVE_SVN} == true ]] && LRC_moveBranchSvn || info "Not moving $BRANCH in repo for LRC"
+[[ ${LRC_DELETE_SHARE} == true ]] && { LRC_deleteBranchShare; LRC_deleteBranchBldShare; } || info "Not deleting $BRANCH on share for LRC"
 
