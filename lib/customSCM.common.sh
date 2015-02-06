4eb70909c1b717c7dda073fad387c48f54622a0b
Bernhard Minks <bernhard.minks.ext@nsn.com>
2015-02-06 14:43:37 +0100

    add LFS post to guess job name

--- lib/customSCM.common.sh
+++ lib/customSCM.common.sh
@@ -80,21 +80,14 @@ getUpstreamProjectName() {
         jobName=${JOB_NAME}
     fi
 
+    local branchName=$(getBranchName)
+
     case ${jobName} in
-        LFS_KNIFE_-_knife_-_Package_-_package)
-            echo LFS_KNIFE_-_Build
-        ;;
-        LFS_CI_-_*_-_Test)
-            local branchName=$(getBranchName)
-            echo LFS_CI_-_${branchName}_-_Package_-_package
-        ;;
-        UBOOT_CI_-_*_-_Test)
-            local branchName=$(getBranchName)
-            echo UBOOT_CI_-_${branchName}_-_Package_-_package
-        ;;
-        *)
-            fatal "no rule to get the upstream job for ${jobName}"
-        ;;
+        LFS_Post_-_*) echo LFS_Prod_-_${branchName}_-_Releasing_-_summary
+        LFS_KNIFE_-_knife_-_Package_-_package) echo LFS_KNIFE_-_Build ;;
+        LFS_CI_-_*_-_Test) echo LFS_CI_-_${branchName}_-_Package_-_package ;;
+        UBOOT_CI_-_*_-_Test) echo UBOOT_CI_-_${branchName}_-_Package_-_package ;;
+        *) fatal "no rule to get the upstream job for ${jobName}" ;;
     esac
 
     return        
