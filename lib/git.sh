d8e761c32669f0e5e7927b9a81352ca874b67052
Bernhard Minks <bernhard@minks.de>
2015-02-09 07:55:49 +0100

    add comments

--- lib/git.sh
+++ lib/git.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+## @file  git.sh
+#  @brief wrapper functions for git
+
+
+## @fn      gitLog()
+#  @brief   get the log from git
+#  @param   {arguments}    arguments for git log
+#  @return  log from git
 gitLog() {
     execute -n git log
 }
 
+## @fn      gitTagAndPushToOrigin()
+#  @brief   creates a tag in git and push the tag to the origin
+#  @param   {tagName}    name of the tag
+#  @return  <none>
 gitTagAndPushToOrigin() {
     local tagName=$1
     mustHaveValue "${tagName}" "tagName"
@@ -12,11 +24,19 @@ gitTagAndPushToOrigin() {
     return
 }
 
+## @fn      gitReset()
+#  @brief   perform a git reset command
+#  @param   {arguments}    arguments for git reset
+#  @return  <none>
 gitReset() {
     execute git reset $@
     return
 }
 
+## @fn      gitDescribe()
+#  @brief   get the description from git
+#  @param   {arguments}    arguments for git describe
+#  @return  description from git
 gitDescribe() {
     execute -n git describe $@
 }
--- lib/try.sh
+++ lib/try.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
-# source: https://stackoverflow.com/questions/22009364/is-there-a-try-catch-command-in-bash
+## @file    try.sh
+#  @brief   a try-catch-implementation for bash
+#  @details source: https://stackoverflow.com/questions/22009364/is-there-a-try-catch-command-in-bash
+#  
+#  example:
+#  try
+#  (
+#     your code, which can fail
+#  )
+#  catch
+#  (
+#     your code
+#  )
+#
 
+## @fn      try()
+#  @brief   try-catch implementation
+#  @param   <none>
+#  @return  <none>
 function try()
 {
     [[ $- = *e* ]]; SAVED_OPT_E=$?
     set +e
 }
 
+## @fn      throw()
+#  @brief   try-catch implementation
+#  @param   <none>
+#  @return  <none>
 function throw()
 {
     exit $1
 }
 
+## @fn      catch()
+#  @brief   try-catch implementation
+#  @param   <none>
+#  @return  <none>
 function catch()
 {
     export ex_code=$?
@@ -20,11 +45,19 @@ function catch()
     return $ex_code
 }
 
+## @fn      throwErrors()
+#  @brief   try-catch implementation
+#  @param   <none>
+#  @return  <none>
 function throwErrors()
 {
     set -e
 }
 
+## @fn      ignoreErrors()
+#  @brief   try-catch implementation
+#  @param   <none>
+#  @return  <none>
 function ignoreErrors()
 {
     set +e
