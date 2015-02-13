7acde78a22925e8f1d4b56e8483a3b3fe88882de
Bernhard Minks <bernhard@minks.de>
2015-02-13 16:45:04 +0100

    wip: ysmv2 first test

---
+++ bin/ysmv2.pl
@@ -0,0 +1,147 @@
+#!/usr/bin/perl 
+package Object;
+
+use strict; 
+use warnings;
+
+sub new {
+    my $class = shift;
+    my $param = { @_ };
+    my $self  = bless $param, $class;
+
+    if( $self->can( "init" ) ) {
+        $self->init();
+    }
+    return $self;
+}
+
+
+package Store::Database;
+use strict;
+use warnings;
+
+use parent qw( -norequire Object );
+
+use DBI;
+use Log::Log4perl qw( :easy);
+
+sub prepare {
+    my $self = shift;
+    my $sql  = shift;
+
+    if( not $self->{dbi} ) {
+
+        my $dbiString = sprintf( "DBI:%s:%s:%s:%s",
+                "mysql",                      # database driver
+                "test_lfspt",                      # database
+                "ulwiki02.emea.nsn-net.net",  # database host
+                3306,                         # database port
+                );
+        my $userName = "lfspt";
+        my $password = "pt";
+        my $dbiArgs  = { AutoCommit => 1,
+                         PrintError => 1 };
+
+        $self->{dbi} = DBI->connect( $dbiString, $userName, $password, $dbiArgs ) or LOGDIE $DBI::errstr;
+    }
+
+    return $self->{dbi}->prepare( $sql ) or LOGDIE $DBI::errstr;
+}
+
+sub reserveTarget {
+    my $self  = shift;
+    my $param = { @_ };
+
+    my $targetName = $param->{targetName} || "not_a_valid_target_name";
+    my $userName   = $param->{userName};
+    my $comment    = $param->{comment};
+
+    my $sth = $self->prepare( 
+        'CALL reserveTarget( ?, ?, ? )'
+    );
+    $sth->execute( $targetName, $userName, $comment ) 
+        or LOGDIE sprintf( "can not reserve target : %s, %s, %s", $targetName, $userName, $comment);
+
+    return;
+}
+
+sub unreserveTarget {
+    my $self  = shift;
+    my $param = { @_ };
+
+    my $targetName = $param->{targetName};
+
+    my $sth = $self->prepare( 
+        'CALL unreserveTarget( ? )'
+    );
+    $sth->execute( $targetName )
+        or LOGDIE sprintf( "can not unreserve target: %s", $targetName );
+
+    return;
+}
+
+sub searchTarget {
+    my $self  = shift;
+    my $param = { @_ };
+
+    my @attributes = @{ $param->{attributes} || [ qw ( not_a_valid_features )] };
+    my $targetName = $param->{targetName};
+
+    my $sqlString = join( " and ", map { sprintf( "target_features like '%%%s%%'", $_ ) } @attributes );
+
+    my $sth = $self->prepare( 
+        "select * from targets where ( ( $sqlString ) or target_name = '$targetName' ) and status = 'free'"
+    );
+    $sth->execute()
+        or LOGDIE sprintf( "can not search target: %s, %s", $targetName, $sqlString );
+    my @results;
+    while ( my $row = $sth->fetchrow_hashref() ) {
+         push @results, $row;
+    }
+    return @results;
+}
+
+package main;
+
+use strict;
+use warnings;
+use Log::Log4perl qw( :easy );
+use Getopt::Long;
+
+my %l4p_config = (
+    'log4perl.category'                  => 'TRACE, Screen',
+    'log4perl.appender.Screen'           => 'Log::Log4perl::Appender::Screen',
+    'log4perl.appender.Screen.stderr'    => '0',
+    'log4perl.appender.Screen.Threshold' => 'TRACE',
+    'log4perl.appender.Screen.layout'    => 'Log::Log4perl::Layout::SimpleLayout',
+);
+
+Log::Log4perl::init( \%l4p_config );
+
+my $opt_targetName = "";
+my @opt_attributes = ();
+my $opt_action = "";
+my $opt_comment = "";
+my $opt_userName = $ENV{"USER"};
+
+GetOptions( 'targetName=s', \$opt_targetName,
+            'action=s',     \$opt_action,
+            'comment=s',    \$opt_comment,
+            'userName=s',   \$opt_userName,
+            'attributes=s', \@opt_attributes,
+        ) or LOGDIE "invalid option";
+
+if( $opt_action eq "reserveTarget" ) {
+    Store::Database->new()->reserveTarget( userName   => $opt_userName,
+                                           comment    => $opt_comment,
+                                           targetName => $opt_targetName );
+} elsif ( $opt_action eq "unreserveTarget" ) {
+    Store::Database->new()->unreserveTarget( userName   => $opt_userName,
+                                             targetName => $opt_targetName );
+} elsif ( $opt_action eq "searchTarget" ) {
+    my @targets = Store::Database->new()->searchTarget( targetName => $opt_targetName, attributes => \@opt_attributes );
+    printf join( "\n", map { $_->{target_name} } @targets );
+    printf "\n";
+}
+
+exit 0;
---
+++ database/ysmv2.sql
@@ -0,0 +1,114 @@
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS targets;
+DROP TABLE IF EXISTS target_status;
+CREATE TABLE target_status (
+    id INT NOT NULL AUTO_INCREMENT,
+    status VARCHAR(10) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX(id)
+);
+
+INSERT INTO target_status ( status ) values ( 'free' );
+INSERT INTO target_status ( status ) values ( 'reserverd' );
+INSERT INTO target_status ( status ) values ( 'maintainance' );
+
+DROP TABLE IF EXISTS targets;
+CREATE TABLE targets (
+    id INT NOT NULL AUTO_INCREMENT,
+    target_name VARCHAR(100) NOT NULL,
+    target_features VARCHAR(1024) ,
+    status VARCHAR(10),
+
+    PRIMARY KEY (id),
+    INDEX(id)
+);
+
+INSERT INTO targets ( target_name, target_features, status  ) values ( 'fcmd15', 'FSM-r2', 'free' );
+INSERT INTO targets ( target_name, target_features, status  ) values ( 'FSCA-1335-X03', 'FSM-r4 FSCA', 'free' );
+INSERT INTO targets ( target_name, target_features, status  ) values ( 'FSPJ-1076-X02', 'FSM-r4 FSPJ', 'free' );
+
+DROP TABLE IF EXISTS booking;
+DROP TABLE IF EXISTS bookings;
+CREATE TABLE bookings (
+    ID INT NOT NULL AUTO_INCREMENT,
+    user VARCHAR(8) NOT NULL,
+    target_id INT NOT NULL,
+    comment VARCHAR(256) NULL,
+    startTime DATETIME NOT NULL,
+    endTime   DATETIME NULL,
+
+    PRIMARY KEY (id),
+    INDEX(id),
+    FOREIGN KEY (target_id)
+        REFERENCES targets(id)
+);
+
+DROP PROCEDURE IF EXISTS reserveTarget;
+DELIMITER //
+CREATE PROCEDURE reserveTarget( IN in_target_name varchar(100), IN in_user varchar(8), IN in_comment varchar(1024) )
+BEGIN
+    DECLARE var_isFree INT;
+    DECLARE var_target_id INT;
+
+    START TRANSACTION WITH CONSISTENT SNAPSHOT;
+    SET autocommit = 0;
+
+    SELECT count(*) INTO var_isFree FROM targets WHERE target_name = in_target_name;
+    IF var_isFree = 0 THEN
+        -- target does not exist
+        ROLLBACK;
+        SIGNAL SQLSTATE '45001'
+        SET MESSAGE_TEXT = 'target does not exist';
+    END IF;
+
+    SELECT count(*) INTO var_isFree FROM targets WHERE target_name = in_target_name and status = 'free';
+
+    IF var_isFree = 1 THEN
+        -- target is free
+        UPDATE targets SET status = 'reserverd' WHERE target_name = in_target_name;
+
+        SELECT id INTO var_target_id FROM targets WHERE target_name = in_target_name;
+        INSERT INTO bookings ( user, target_id, comment, startTime ) VALUES ( in_user, var_target_id, in_comment, NOW() );
+        COMMIT;
+    ELSE
+        -- target is not free => raise an error
+        ROLLBACK;
+        SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = "target is not free";
+    END IF;
+
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS unreserveTarget;
+DELIMITER //
+CREATE PROCEDURE unreserveTarget( IN in_target_name varchar(100) )
+BEGIN
+    DECLARE var_target_id INT;
+
+    START TRANSACTION WITH CONSISTENT SNAPSHOT;
+
+    SET autocommit = 0;
+
+    SELECT id INTO var_target_id FROM targets WHERE target_name = in_target_name;
+    UPDATE bookings SET endTime = NOW() WHERE target_id = var_target_id AND endTime IS NULL;
+    UPDATE targets SET status = 'free' WHERE id = var_target_id;
+
+    COMMIT;
+
+END //
+DELIMITER ;
+
+select * from targets;
+select * from bookings;
+
+CALL reserveTarget( 'fcmd15', 'bm', 'foobar' );
+
+select * from targets;
+select * from bookings;
+CALL reserveTarget( 'fcmd15', 'bm', 'foobar' );
+CALL reserveTarget( 'abc', 'bm', 'foobar' );
+select * from targets;
+select * from bookings;
+CALL unreserveTarget( 'fcmd15' );
+select * from targets;
+select * from bookings;
