4a09ac35f1df2afbe492576fa44c614b1f540df2
Bernhard Minks <bernhard@minks.de>
2015-03-02 20:44:44 +0100

    renamed ut mustHaveSdkOnShare

--- test/16_createWorkspace_mustHaveLocalSdks.sh
+++ test/16_createWorkspace_mustHaveLocalSdks.sh
@@ -1,98 +1,76 @@
 #!/bin/bash
 
-#  not ready yet
-exit 0
+source test/common.sh
 
-source lib/common.sh
-initTempDirectory
+source lib/package.sh
 
-source lib/createWorkspace.sh
-
-export UNITTEST_COMMAND=$(createTempFile)
-export SHARE=$(createTempDirectory)
+export UT_MOCKED_COMMANDS=$(createTempFile)
 
 oneTimeSetUp() {
     mockedCommand() {
-        echo "$@" >> ${UNITTEST_COMMAND}
+        echo "$@" >> ${UT_MOCKED_COMMANDS}
     }
-    exit_handler() {
-        echo exit
-    }
-    mustHaveValue() {
-        mockedCommand "mustHaveValue $@"
-        return
-    }
-    execute() {
-        mockedCommand "execute $@"
-    }
-    createOrUpdateWorkspace() {
-        mockedCommand "createOrUpdateWorkspace $@"
-    }
-
-#     getConfig() {
-#         mockedCommand "getConfig $@"
-#         case $1 in
-#             LFS_CI_uc_klocwork_project_name)         echo PS_LFS_DDAL ;;
-#             LFS_CI_uc_klocwork_port)                 echo 12345 ;;
-#             LFS_CI_uc_klocwork_hostname)             echo klocwork_hostname ;;
-#             LFS_CI_uc_klocwork_licence_port)         echo 54321 ;;
-#             LFS_CI_uc_klocwork_licence_host)         echo licence_hostname ;;
-#             LFS_CI_uc_klocwork_cmd_kwinject)         echo /path/to/bin/kwinject ;;
-#             LFS_CI_uc_klocwork_cmd_kwadmin)          echo /path/to/bin/kwadmin ;;
-#             LFS_CI_uc_klocwork_cmd_kwbuildproject)   echo /path/to/bin/kwbuildproject ;;
-#             LFS_CI_uc_klocwork_url)                  echo http://klocwork_hostname:12345/ ;;
-#             LFS_CI_uc_klocwork_template)             echo kw_template ;;
-#             LFS_CI_uc_klocwork_tables)               echo kw_tables ;;
-#             LFS_CI_uc_klocwork_architectures)        echo arm powerpc mips i386 x64 ;;
-#             LFS_CI_uc_klocwork_report_python_script) echo http://svn/path/to/script.py ;;
-#             LFS_CI_uc_klocwork_python_home)          echo /path/to/python/home ;;
-#         esac
-#     }
-
     svnExport() {
         mockedCommand "svnExport $@"
     }
-
+    getConfig() {
+        mockedCommand "getConfig $@"
+        case $1 in 
+            LFS_CI_UC_package_copy_to_share_name) echo ${UT_SDK_SHARE}  ;;
+            LFS_CI_UC_package_sdk_svn_location)   echo http://svnSDKurl ;;
+        esac
+    }
+    mustExistDirectory() {
+        mockedCommand "mustExistDirectory $@"
+    }
     return
 }
 
 setUp() {
-    cp -f /dev/null ${UNITTEST_COMMAND}
-    mkdir -p ${SHARE}/build/home/path/to/baseline1
-    mkdir -p ${SHARE}/build/home/path/to/baseline2
-}
-
-tearDown() {
-    rm -rf ${UNITTEST_COMMAND}
-    rm -rf ${CI_LOGGING_LOGFILENAME}
-    rm -rf ${SHARE}
+    cp -f /dev/null ${UT_MOCKED_COMMANDS}
+    export JOB_NAME=LFS_CI_-_trunk_-_Build_-_abc_-_abc
     return
 }
 
-testUsecaseKlocwork_part1() {
-    export WORKSPACE=$(createTempDirectory)
-    export LFS_CI_SHARE_MIRROR=$(createTempDirectory)
+tearDown() {
+    rm -rf ${UT_MOCKED_COMMANDS}
+    return
+}
 
-    export BUILD_ID=1234
+test1() {
+    export UT_SDK_SHARE=$(createTempDirectory)
 
-    mkdir -p ${WORKSPACE}/workspace/bld
-    ln -sf ${SHARE}/build/home/path/to/baseline1 ${WORKSPACE}/workspace/bld/baseline1
+    assertTrue "mustHaveSdkOnShare SDK_BASELINE"
 
-    mustHaveLocalSdks
+    local expect=$(createTempFile)
+    cat <<EOF > ${expect}
+getConfig LFS_CI_UC_package_copy_to_share_name
+mustExistDirectory ${UT_SDK_SHARE}
+mustExistDirectory ${UT_SDK_SHARE}/SDKs
+getConfig LFS_CI_UC_package_sdk_svn_location
+svnExport http://svnSDKurl/tags/SDK_BASELINE ${UT_SDK_SHARE}/SDKs/SDK_BASELINE
+EOF
+    assertExecutedCommands ${expect}
 
-    # assertTrue "mustHaveLocalSdks"
-    cat ${UNITTEST_COMMAND}
+    return
+}
 
-    find ${SHARE}
-    find ${LFS_CI_SHARE_MIRROR}
-    find ${WORKSPACE}
-    ls -larR ${WORKSPACE}
+test2() {
+    export UT_SDK_SHARE=$(createTempDirectory)
+    mkdir -p ${UT_SDK_SHARE}/SDKs
+    mkdir -p ${UT_SDK_SHARE}/SDKs/SDK_BASELINE
 
-#     local expect=$(createTempFile)
-#     cat <<EOF > ${expect}
-# EOF
-#     assertEquals "$(cat ${expect})" "$(cat ${UNITTEST_COMMAND})"
+    assertTrue "mustHaveSdkOnShare SDK_BASELINE"
 
+    local expect=$(createTempFile)
+    cat <<EOF > ${expect}
+getConfig LFS_CI_UC_package_copy_to_share_name
+mustExistDirectory ${UT_SDK_SHARE}
+mustExistDirectory ${UT_SDK_SHARE}/SDKs
+EOF
+    assertExecutedCommands ${expect}
+
+    return
 }
 
 source lib/shunit2
