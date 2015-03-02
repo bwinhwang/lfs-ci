7c4877e4d30d422a0022797e8c1bc9f11f5d3d34
Bernhard Minks <bernhard.minks.ext@nokia.com>
2015-03-02 20:28:18 +0100

    exclude rootfs.d from results/psl/platforms

--- lib/uc_lfs_package.sh
+++ lib/uc_lfs_package.sh
@@ -82,7 +82,7 @@ copyGenericBuildResults() {
     for bldDirectory in ${workspace}/bld/bld-*-*/results/psl/platforms ; do
         [[ -d ${bldDirectory} ]] || continue
         info "copy generic build results from ${bldDirectory} to ${dst}"
-        execute rsync -av --exclude=.svn ${bldDirectory}/ ${dst}/
+        execute rsync -av --exclude=rootfs.d --exclude=.svn ${bldDirectory}/ ${dst}/
     done
     return
 }
