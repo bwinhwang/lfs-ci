156a3274f95baaa69284f421633980abe03a5f60
Bernhard Minks <bernhard@minks.de>
2015-04-20 08:06:05 +0200

    fixes

--- lib/artifacts.sh
+++ lib/artifacts.sh
@@ -189,6 +189,8 @@ copyArtifactsToWorkspace() {
         local result=$(echo ${jobData} | cut -d: -f 2)
         local name=$(  echo ${jobData} | cut -d: -f 3-)
 
+        [[ ${result} = NOT_BUILT ]] && continue
+
         trace "jobName ${name} buildNumber ${nuber} jobResult ${result}"
 
         if [[ ${result} != "SUCCESS" && ${result} != "NOT_BUILT" ]] ; then
