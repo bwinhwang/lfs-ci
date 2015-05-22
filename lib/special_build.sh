1f773ce2a525071b90b19942c1535aae6cca9c19
Bernhard Minks <bernhard@minks.de>
2015-05-22 10:09:28 +0200

    wip: apply patch in the correct way

--- lib/special_build.sh
+++ lib/special_build.sh
@@ -217,8 +217,12 @@ applyKnifePatches() {
 
         if [[ -e ${workspace}/bld/bld-${type}-input/lfs.patch ]] ; then
             info "applying lfs.patch file..."
-            # error will be ignored, if the patch file will not apply without problems
-            execute -i patch -p0 -d ${workspace} < ${workspace}/bld/bld-${type}-input/lfs.patch
+            # apply patch only on files which exists in workspace
+            for fileInPatch in $(execute -n lsdiff ${workspace}/bld/bld-${type}-input/lfs.patch) ; do
+                local tmpPatchFile=$(createTempFile)
+                execute -n filterdiff -i ${fileInPatch} > ${tmpPatchFile}
+                execute -i patch -p0 -d ${workspace} < ${tmpPatchFile}
+            done                     
         fi
     done
 
--- test/28_uc_knife_build_applyKnifePatches.sh
+++ test/28_uc_knife_build_applyKnifePatches.sh
@@ -23,26 +23,38 @@ oneTimeTearDown() {
 
 setUp() {
     rm -rf ${UT_MOCKED_COMMANDS}
-}
-tearDown() {
-    true 
-}
-
-test1() {
     export WORKSPACE=$(createTempDirectory)
     export KNIFE_LFS_BASELINE=PS_LFS_OS_2014_01_0001
     export UPSTREAM_PROJECT=upstream_project
     export UPSTREAM_BUILD=123
     export JOB_NAME=LFS_KNIFE_-_knife_-_Build
 
+    mkdir -p ${WORKSPACE}/workspace/src-fsmpsl
+    touch    ${WORKSPACE}/workspace/src-fsmpsl/Buildfile
+    touch    ${WORKSPACE}/workspace/src-fsmpsl/Dependencies
+    mkdir -p ${WORKSPACE}/workspace/src-rfs
+    touch    ${WORKSPACE}/workspace/src-rfs/Buildfile
+    touch    ${WORKSPACE}/workspace/src-rfs/Dependencies
+    mkdir -p ${WORKSPACE}/workspace/src-psl
+    touch    ${WORKSPACE}/workspace/src-psl/Buildfile
+    touch    ${WORKSPACE}/workspace/src-psl/Dependencies
+
     mkdir -p ${WORKSPACE}/workspace/bld/bld-knife-input/
-    touch ${WORKSPACE}/workspace/bld/bld-knife-input/lfs.tar.gz 
+    cp ${LFS_CI_ROOT}/test/data/28_uc_knife_build_applyKnifePatches.patch \
+             ${WORKSPACE}/workspace/bld/bld-knife-input/lfs.patch
+    touch    ${WORKSPACE}/workspace/bld/bld-knife-input/lfs.tar.gz
 
+}
+tearDown() {
+    true 
+}
+
+test1() {
     assertTrue "applyKnifePatches"
-
     local expect=$(createTempFile)
 cat <<EOF > ${expect}
 execute tar -xvz -C ${WORKSPACE}/workspace -f ${WORKSPACE}/workspace/bld/bld-knife-input/lfs.tar.gz
+execute -i patch -p0 -d ${WORKSPACE}/workspace
 EOF
     assertExecutedCommands ${expect}
 
@@ -50,22 +62,10 @@ EOF
 }
 
 test2() {
-    export WORKSPACE=$(createTempDirectory)
-    export KNIFE_LFS_BASELINE=PS_LFS_OS_2014_01_0001
-    export UPSTREAM_PROJECT=upstream_project
-    export UPSTREAM_BUILD=123
-    export JOB_NAME=LFS_KNIFE_-_knife_-_Build
-
-    mkdir -p ${WORKSPACE}/workspace/bld/bld-knife-input/
-    touch ${WORKSPACE}/workspace/bld/bld-knife-input/lfs.patch
-    touch ${WORKSPACE}/workspace/bld/bld-knife-input/lfs.tar.gz 
-
     assertTrue "applyKnifePatches"
 
     local expect=$(createTempFile)
 cat <<EOF > ${expect}
-execute tar -xvz -C ${WORKSPACE}/workspace -f ${WORKSPACE}/workspace/bld/bld-knife-input/lfs.tar.gz
-execute -i patch -p0 -d ${WORKSPACE}/workspace
 EOF
     assertExecutedCommands ${expect}
 
