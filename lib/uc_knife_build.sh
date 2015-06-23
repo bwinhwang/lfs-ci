c8e8908ccad5ca46a6815d4654ebd3b530626822
Erwin Ambrosch <erwin.ambrosch.ext@nokia.com>
2015-06-23 10:42:39 +0200

    enabled knife tests.

--- lib/uc_knife_build.sh
+++ lib/uc_knife_build.sh
@@ -132,68 +132,66 @@ usecase_LFS_KNIFE_PACKAGE() {
 #  @brief  interface from/to WFT for LFS knifing
 #  @param  <none>
 #  @param  <none>
-usecase_LFS_KNIFE_WFT_TRIGGER() {
-    initTempDirectory
-    local zipFile="knife.zip"
-    local xmlFile=$(createTempFile)
-    local knifeId=$KNIFE_ID
-    local wftHostAddress=$(getConfig WORKFLOWTOOL_hostname)
-    local wftApiKey=$(getConfig WORKFLOWTOOL_api_key)
-    mustHaveValue "${knifeId}" "knifeId"
-    mustHaveValue "${wftHostAddress}" "wftHostAddress"
-    mustHaveValue "${wftApiKey}" "wftApiKey"
-    mustHaveValue "${WORKSPACE}" "WORKSPACE"
-
-    info "kinfeId: ${knifeId}"
-
-    # Get XML from WFT
-    curl -k ${wftHostAddress}/ext/knife/${knifeId}/info -o ${xmlFile}
-
-    info "WFT xml file\n: ${xmlFile}"
-
-    # Get info from XML and set build description
-    local ftpPath=$(${LFS_CI_ROOT}/bin/xpath -e "/knife/dir/text()" $xmlFile)
-    local requestor=$(${LFS_CI_ROOT}/bin/xpath -e "/knife/requestor/text()" $xmlFile)
-    local lfsBaseline=$(${LFS_CI_ROOT}/bin/xpath -e "/knife/baseline/text()" $xmlFile)
-    setBuildDescription "${JOB_NAME}" "${BUILD_NUMBER}" "knife id: ${knifeId}, requestor: ${requestor}, lfs baseline: ${lfsBaseline}"
-
-    # wft api: set knife build as started
-    curl -k ${wftHostAddress}/ext/knife/${knifeId}/started?access_key=${wftApiKey}
-
-    # get knife.zip from rotta location via ftp
-    # check if exists
-    # unpack knife.zip file
-    source ${LFS_CI_ROOT}/lib/autoftp.sh
-    ftpPath=$(echo $ftpPath | cut -d'\' -f5- | sed -e 's,\\,/,')
-    cd $WORKSPACE
-    ftpGet $ftpPath ${zipFile}
-    [[ ! -e ${zipFile} ]] && error { "File ${zipFile} does not exist."; exit 1; }
-    unzip ${zipFile}
-
-    # trigger lfs knife build job with required parameters
-    mustHaveValue "$KNIFE_BUILD_JOB" "KNIFE_BUILD_JOB"
-    local jenkinsCmdl=$(getConfig jenkinsCli)
-    local jenkinsMasterAddress=$(getConfig jenkinsMasterServerHttpsUrl)
-    info "Trigger jenkins job $KNIFE_BUILD_JOB and wait until it is finished."
-    local jobResult=$(java -jar ${jenkinsCmdl} -s ${jenkinsMasterAddress} build $KNIFE_BUILD_JOB -p "lfs.patch=${WORKSPACE}/lfs.patch" -p "KNIFE_LFS_BASELINE=${lfsBaseline}" -s)
-
-    jobNumber=$(echo ${jobResult} | awk -F'#' '{print $1}' | sed -e 's/#//')
-    jobStatus=$(echo ${jobResult} | awk '{print $8}')
-
-    if [[ "${jobStatus}" == "FAILURE" ]]; then
-        curl -k ${wftHostAddress}/ext/knife/${knifeId}/failed?access_key=${wftApiKey}&result_url=${jenkinsMasterAddress}/job/${KNIFE_BUILD_JOB}/${jobNumber}
-        error "Jenkins knife build Job failed: ${jenkinsMasterAddress}/job/${KNIFE_BUILD_JOB}/${jobNumber}"
-        exit 1
-    elif [[ "${jobStatus}" == "SUCCESS" ]]; then
-        curl -k ${wftHostAddress}/ext/knife/${knifeId}/succeeded?access_key=${wftApiKey}
-        info "Jenkins knife build Job succeeded: ${jenkinsMasterAddress}/job/${KNIFE_BUILD_JOB}/${jobNumber}"
-    else
-        error "Unkown status of Job $KNIFE_BUILD_JOB"
-        exit 1
-    fi
-
-    # * get artifacts / location of the results of the knife
-
-    # * wft api: push (location of the results of the knife) s3 url to wft 
-    ### Please note, that currently S3 URL can be stored starting with next WFT version (after 29th of june)
-}
+#usecase_LFS_KNIFE_WFT_TRIGGER() {
+#    initTempDirectory
+#    local zipFile="knife.zip"
+#    local xmlFile=$(createTempFile)
+#    local knifeId=$KNIFE_ID
+#    local wftHostAddress=$(getConfig WORKFLOWTOOL_hostname)
+#    local wftApiKey=$(getConfig WORKFLOWTOOL_api_key)
+#    mustHaveValue "${knifeId}" "knifeId"
+#    mustHaveValue "${wftHostAddress}" "wftHostAddress"
+#    mustHaveValue "${wftApiKey}" "wftApiKey"
+#    mustHaveValue "${WORKSPACE}" "WORKSPACE"
+#
+#    info "kinfeId: ${knifeId}"
+#
+#    # Get XML from WFT
+#    curl -k ${wftHostAddress}/ext/knife/${knifeId}/info -o ${xmlFile}
+#
+#    info "WFT xml file\n: ${xmlFile}"
+#
+#    # Get info from XML and set build description
+#    local ftpPath=$(${LFS_CI_ROOT}/bin/xpath -e "/knife/dir/text()" $xmlFile)
+#    local requestor=$(${LFS_CI_ROOT}/bin/xpath -e "/knife/requestor/text()" $xmlFile)
+#    local lfsBaseline=$(${LFS_CI_ROOT}/bin/xpath -e "/knife/baseline/text()" $xmlFile)
+#    setBuildDescription "${JOB_NAME}" "${BUILD_NUMBER}" "knife id: ${knifeId}, requestor: ${requestor}, lfs baseline: ${lfsBaseline}"
+#
+#    # wft api: set knife build as started
+#    curl -k ${wftHostAddress}/ext/knife/${knifeId}/started?access_key=${wftApiKey}
+#
+#    # get knife.zip from rotta location via ftp
+#    # check if exists
+#    # unpack knife.zip file
+#    source ${LFS_CI_ROOT}/lib/autoftp.sh
+#    ftpPath=$(echo $ftpPath | cut -d'\' -f5- | sed -e 's,\\,/,')
+#    cd $WORKSPACE
+#    ftpGet $ftpPath ${zipFile}
+#    [[ ! -e ${zipFile} ]] && error { "File ${zipFile} does not exist."; exit 1; }
+#    unzip ${zipFile}
+#
+#    # trigger lfs knife build job with required parameters
+#    mustHaveValue "$KNIFE_BUILD_JOB" "KNIFE_BUILD_JOB"
+#    local jenkinsCmdl=$(getConfig jenkinsCli)
+#    local jenkinsMasterAddress=$(getConfig jenkinsMasterServerHttpsUrl)
+#    info "Trigger jenkins job $KNIFE_BUILD_JOB and wait until it is finished."
+#    local jobResult=$(java -jar ${jenkinsCmdl} -s ${jenkinsMasterAddress} build $KNIFE_BUILD_JOB -p "lfs.patch=${WORKSPACE}/lfs.patch" -p "KNIFE_LFS_BASELINE=${lfsBaseline}" -s)
+#
+#    jobNumber=$(echo ${jobResult} | awk -F'#' '{print $1}' | sed -e 's/#//')
+#    jobStatus=$(echo ${jobResult} | awk '{print $8}')
+#
+#    if [[ "${jobStatus}" == "FAILURE" ]]; then
+#        curl -k ${wftHostAddress}/ext/knife/${knifeId}/failed?access_key=${wftApiKey}&result_url=${jenkinsMasterAddress}/job/${KNIFE_BUILD_JOB}/${jobNumber}
+#        error "Jenkins knife build Job failed: ${jenkinsMasterAddress}/job/${KNIFE_BUILD_JOB}/${jobNumber}"
+#        exit 1
+#    elif [[ "${jobStatus}" == "SUCCESS" ]]; then
+#        curl -k ${wftHostAddress}/ext/knife/${knifeId}/succeeded?access_key=${wftApiKey}
+#        info "Jenkins knife build Job succeeded: ${jenkinsMasterAddress}/job/${KNIFE_BUILD_JOB}/${jobNumber}"
+#    else
+#        error "Unkown status of Job $KNIFE_BUILD_JOB"
+#        exit 1
+#    fi
+#
+#    # * wft api: push (location of the results of the knife) s3 url to wft  -> in den artifacts fom knife package job (readme file).
+#    ### Please note, that currently S3 URL can be stored starting with next WFT version (after 29th of june)
+#}
