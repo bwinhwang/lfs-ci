48c7008943600e37d48b993c239f1eb9dbb52088
Bernhard Minks <bernhard.minks.ext@nokia.com>
2015-06-30 17:13:06 +0200

    fix ut

--- test/32_uc_pkgpool_usecase_PKGPOOL_BUILD.sh
+++ test/32_uc_pkgpool_usecase_PKGPOOL_BUILD.sh
@@ -69,7 +69,6 @@ gitDescribe --abbrev=0
 gitTagAndPushToOrigin PKGPOOL_FOO
 execute -n git rev-parse HEAD
 setBuildDescription PKGPOOL_CI_-_trunk_-_Build 123 PKGPOOL_FOO
-execute touch /build/home/psulm/SC_LFS/pkgpool/.hashpool
 execute -n sed -ne s|^src [^ ]* \(.*\)$|PS_LFS_PKG = \1|p ${WORKSPACE}/workspace/pool/*.meta
 createArtifactArchive 
 EOF
