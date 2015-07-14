4eabca6963662c9cbc13d260c7c3ba11c51756c8
Bernhard Minks <bernhard.minks.ext@nokia.com>
2015-07-14 16:01:59 +0200

    disable smoketest, if smoketest fails, option to move target to repair center, if test fails

--- test/40_booking_unreserveTarget.sh
+++ test/40_booking_unreserveTarget.sh
@@ -11,6 +11,9 @@ oneTimeSetUp() {
     execute() {
         mockedCommand "execute $@"
     }
+    getConfig() {
+        echo ${LFS_CI_BOOKING_MOVE_TARGET_TO_REPAIR_CENTER}
+    }
 
     return
 }
@@ -27,7 +30,62 @@ tearDown() {
 }
 
 test1() {
+    unset LFS_CI_BOOKING_MOVE_TARGET_TO_REPAIR_CENTER
+    export LFS_CI_BOOKING_MOVE_TARGET_TO_REPAIR_CENTER
     assertTrue "unreserveTarget"
+
+    local expect=$(createTempFile)
+    cat <<EOF > ${expect}
+execute ${LFS_CI_ROOT}/bin/unreserveTarget --targetName=targetName
+EOF
+    assertExecutedCommands ${expect}
+
+    return
+}
+
+test2() {
+    unset LFS_CI_BOOKING_MOVE_TARGET_TO_REPAIR_CENTER
+    export LFS_CI_BOOKING_MOVE_TARGET_TO_REPAIR_CENTER
+    assertTrue "unreserveTarget 1"
+
+    local expect=$(createTempFile)
+    cat <<EOF > ${expect}
+execute ${LFS_CI_ROOT}/bin/unreserveTarget --targetName=targetName
+EOF
+    assertExecutedCommands ${expect}
+
+    return
+}
+test3() {
+    export LFS_CI_BOOKING_MOVE_TARGET_TO_REPAIR_CENTER=1
+    assertTrue "unreserveTarget 1"
+
+    local expect=$(createTempFile)
+    cat <<EOF > ${expect}
+execute ${LFS_CI_ROOT}/bin/unreserveTarget --targetName=targetName
+execute ${LFS_CI_ROOT}/bin/reserveTarget --targetName=targetName --userName=doRepair --comment=red target by no job name / no build number
+EOF
+    assertExecutedCommands ${expect}
+
+    return
+}
+test4() {
+    unset LFS_CI_BOOKING_MOVE_TARGET_TO_REPAIR_CENTER
+    export LFS_CI_BOOKING_MOVE_TARGET_TO_REPAIR_CENTER
+    assertTrue "unreserveTarget 0"
+
+    local expect=$(createTempFile)
+    cat <<EOF > ${expect}
+execute ${LFS_CI_ROOT}/bin/unreserveTarget --targetName=targetName
+EOF
+    assertExecutedCommands ${expect}
+
+    return
+}
+
+test5() {
+    export LFS_CI_BOOKING_MOVE_TARGET_TO_REPAIR_CENTER=1
+    assertTrue "unreserveTarget 0"
 
     local expect=$(createTempFile)
     cat <<EOF > ${expect}
