d6b96bf7d53c848dccf416b65c2425b0cfa8b8aa
Bernhard Minks <bernhard.minks.ext@nokia.com>
2015-07-30 11:36:55 +0200

    add documentation

--- lib/uc_admin_mysql_backup.sh
+++ lib/uc_admin_mysql_backup.sh
@@ -5,6 +5,14 @@
 [[ -z ${LFS_CI_SOURCE_database} ]] && source ${LFS_CI_ROOT}/lib/database.sh
 [[ -z ${LFS_CI_SOURCE_common}   ]] && source ${LFS_CI_ROOT}/lib/common
 
+## @fn      usecase_ADMIN_MYSQL_BACKUP()
+#  @brief   run usecase ADMIN_MYSQ_BACKUP
+#  @details dumping the database into a file called dump.sql and store it
+#           (uncompressed) in git. So we can store several 1000 versions
+#           without wasting too much space on the harddisk.
+#           15000 versions need 3.7 GB.
+#  @param   <none>
+#  @return  <none>
 usecase_ADMIN_MYSQL_BACKUP() {
 
     mustHaveDatabaseCredentials
@@ -12,6 +20,10 @@ usecase_ADMIN_MYSQL_BACKUP() {
     execute mkdir -p ${HOME}/mysql_backup
     execute cd ${HOME}/mysql_backup
     [[ ! -d .git ]] && execute git init
+
+    # there is a problem between the version of mysqldump on the database server and the client (here master).
+    # see https://bugs.mysql.com/bug.php?id=66765
+    # workaround: run mysqldump on the database host
     execute ssh ${dbHost} mysqldump -h ${dbHost} -u ${dbUser} -p${dbPass} --routines ${dbName} --result-file=${HOME}/mysql_backup/dump.sql 
     execute git add dump.sql 
     execute git commit -q -m "backup $@"
