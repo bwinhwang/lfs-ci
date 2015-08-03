955df643bb90640a336545c36a7347883fbdda6d
Erwin Ambrosch <erwin.ambrosch.ext@nokia.com>
2015-08-03 08:38:51 +0200

    setup sandbox: docu, added remove_sandbox function, testing,...

--- sandbox/setup-sandbox.sh
+++ sandbox/setup-sandbox.sh
@@ -13,16 +13,18 @@ ITSME=$(basename $0)
 BRANCH_VIEWS="trunk"
 NESTED_VIEWS="DEV/Developer_Build,DEV/Knife_alpha"
 ROOT_VIEWS="ADMIN,UBOOT"
-DISABLE_BUILD_JOBS=true
-LRC=true
-KEEP_JENKINS_PLUGINS=false
+DISABLE_BUILD_JOBS="true"
+LRC="true"
+KEEP_JENKINS_PLUGINS="false"
 WORK_DIR="/var/fpwork"
 LOCAL_WORK_DIR=${WORK_DIR}
 CI_USER="psulm"
 LRC_CI_USER="ca_lrcci"
+LFS_CI_ROOT="${HOME}/lfs-ci"
 SANDBOX_USER=${USER}
+PURGE_SANDBOX="false"
 
-while getopts ":r:n:b:d:i:c:h:elp" OPT; do
+while getopts ":r:n:b:w:i:c:h:s:delp" OPT; do
     case ${OPT} in
         b)
             BRANCH_VIEWS=$OPTARG
@@ -33,7 +35,7 @@ while getopts ":r:n:b:d:i:c:h:elp" OPT; do
         r)
             ROOT_VIEWS=$OPTARG
         ;;
-        d)
+        w)
             LOCAL_WORK_DIR=$OPTARG
         ;;
         i)
@@ -42,14 +44,20 @@ while getopts ":r:n:b:d:i:c:h:elp" OPT; do
         c)
             LRC_CI_USER=$OPTARG
         ;;
+        s)
+            LFS_CI_ROOT=$OPTARG
+        ;;
+        d)
+            PURGE_SANDBOX="true"
+        ;;
         e)
-            DISABLE_BUILD_JOBS=false
+            DISABLE_BUILD_JOBS="false"
         ;;
         l)
-            LRC=false
+            LRC="false"
         ;;
         p)
-            KEEP_JENKINS_PLUGINS=true
+            KEEP_JENKINS_PLUGINS="true"
         ;;
         h)
             help
@@ -58,46 +66,49 @@ while getopts ":r:n:b:d:i:c:h:elp" OPT; do
     esac
 done
 
+JENKINS_DIR="lfs-jenkins" # subdirectory within $WORK_DIR/$USER
 PROD_JENKINS_SERVER="lfs-ci.emea.nsn-net.net"
-PROD_JENKINS_HOME="${WORK_DIR}/${CI_USER}/lfs-jenkins/home"
+PROD_JENKINS_HOME="${WORK_DIR}/${CI_USER}/${JENKINS_DIR}/home"
 PROD_JENKINS_JOBS="${PROD_JENKINS_HOME}/jobs"
 
 LRC_PROD_JENKINS_SERVER="lfs-lrc-ci.int.net.nokia.com"
-LRC_PROD_JENKINS_HOME="${WORK_DIR}/${LRC_CI_USER}/lfs-jenkins/home"
+LRC_PROD_JENKINS_HOME="${WORK_DIR}/${LRC_CI_USER}/${JENKINS_DIR}/home"
 LRC_PROD_JENKINS_JOBS="${LRC_PROD_JENKINS_HOME}/jobs"
 
 HTTP_ADDRESS="0.0.0.0"
 HTTP_PORT="8090"
 JENKNIS_VERSION="1.532.3"
-JENKINS_HOME="${LOCAL_WORK_DIR}/${SANDBOX_USER}/lfs-jenkins/home"
+JENKINS_HOME="${LOCAL_WORK_DIR}/${SANDBOX_USER}/${JENKINS_DIR}/home"
 JENKINS_PLUGINS="${JENKINS_HOME}/plugins"
 JVM_OPTS="-Djava.io.tmpdir=/var/tmp"
 JENKINS_OPTS="--httpListenAddress=${HTTP_ADDRESS} --httpPort=${HTTP_PORT}"
-LFS_CI_ROOT="${HOME}/lfs-ci"
 JENKINS_WAR="${LFS_CI_ROOT}/lib/java/jenkins/jenkins-${JENKNIS_VERSION}.war"
 JENKINS_CLI_JAR="${LFS_CI_ROOT}/lib/java/jenkins/jenkins-cli-${JENKNIS_VERSION}.jar"
 LFS_CI_CONFIG_FILE="${LFS_CI_ROOT}/etc/development.cfg"
 PID_FILE="${JENKINS_HOME}/jenkins.pid"
 LOG_FILE="${JENKINS_HOME}/jenkins.log"
+GIT_URL="ssh://git@psulm.nsn-net.net/projects/lfs-ci.git"
+SANDBOX_SCRIPT_DIR="${LFS_CI_ROOT}/sandbox"
 HOST=$(hostname)
 TMP="/tmp"
-GIT_URL="ssh://git@psulm.nsn-net.net/projects/lfs-ci.git"
+
+echo "---> $LOCAL_WORK_DIR"
 
 help() {
 cat << EOF
 
     Script in order to setup a LFS CI sandbox on the local machine.
 
-    Jenkins will be installed into directory ${LOCAL_WORK_DIR}/\$USER/lfs-jenkins/home in
-    which \$LOCAL_WORK_DIR defaults to /var/fpwork. This can be overridden by -d LOCAL_WORK_DIR.
-    Be aware, that \$USER is always appended to \$WORK_DIR.
-    CI scripting will be cloned to \${LFS_CI_ROOT} pointing to branch development.
+    Jenkins will be installed into directory \${LOCAL_WORK_DIR}/$USER/${JENKINS_DIR}/home. This
+    can be overridden by using -w LOCAL_WORK_DIR.
+    CI scripting will be cloned to \${LFS_CI_ROOT} pointing to branch development in which
+    \${LFS_CI_ROOT} defaults to \${USER}/lfs-ci but can be overriden by -s LFS_CI_ROOT.
     Job and view configuration are copied from Jenkins server ${PROD_JENKINS_SERVER}
     or ${PROD_JENKINS_SERVER} respectively. Per default all .*_Build$ Jobs will be 
     disabled in the sandbox. If you want them enabled use the -e flag. LRC trunk is 
-    copied per default except -p flag is present.
+    copied per default except -l flag is present.
 
-    After the script has been finished check the system settings of the new sandbox.
+    After the script has been finished check the system settings of the new sandbox Jenkins.
 
     Example: $ITSME -r UBOOT -n DEV/Developer_Build -b trunk,FB1503 -l -p
              --> Copy UBOOT, Developer_Build (within DEV view) and trunk. Skip creating LRC on sandbox (-l).
@@ -107,9 +118,11 @@ cat << EOF
         -b Comma separated list of BRANCHES to be copied from LFS CI (not LRC branches). Defaluts to ${BRANCH_VIEWS}.
         -n Comma separated list of nested view that should be created in sandbox. Defaluts to ${NESTED_VIEWS}.
         -r Comma separated list of root views (top level tabs) that should be created in sandbox. Defaults to ${ROOT_VIEWS}.
-        -d Specify \$LOCAL_WORK_DIR directory for Jenkins installation. Defaults to ${LOCAL_WORK_DIR}.
+        -w Specify \$LOCAL_WORK_DIR directory for Jenkins installation. Defaults to ${LOCAL_WORK_DIR}.
         -i lfs ci user. Defaults to ${CI_USER}.
         -c lfs lrc ci user. Defaults to ${LRC_CI_USER}.
+        -s root dirctory of lfs ci scripting. Defaults to ${LFS_CI_ROOT}.
+        -d delete local sandbox installation. Works for standard installation only (-w and -s were not used for installation).
         -e (flag) Disable all .*_Build$ jobs in sandbox. Defaults to ${DISABLE_BUILD_JOBS}.
         -l (flag) Create LRC trunk within sandbox. Defaults to ${LRC}.
         -p (flag) Keep jenkins plugins from existing sandbox installation. Defaults to ${KEEP_JENKINS_PLUGINS}.
@@ -123,17 +136,24 @@ EOF
 #  @return  <none>
 pre_actions() {
     if [[ $(ps aux | grep java | grep jenkins) ]]; then
-        echo -e "\n\tERROR: Jenkins is sill running.\n"
+        echo "ERROR: Jenkins is sill running."
         exit 1
     fi
     if [[ -d ${LOCAL_WORK_DIR} && ! -w ${LOCAL_WORK_DIR} ]]; then
-        echo -e "\n\tERROR: Ensure that ${LOCAL_WORK_DIR} is writable for user ${SANDBOX_USER}.\n"
+        echo "ERROR: Ensure that ${LOCAL_WORK_DIR} is writable for user ${SANDBOX_USER}."
         exit 2
+    fi
+    if [[ "${KEEP_JENKINS_PLUGINS}" == "true" && ! -d ${JENKINS_PLUGINS} ]]; then
+        echo "ERROR: -p makes no sense because there is no ${JENKINS_PLUGINS}"
+        exit 3
     fi
 }
 
 ## @fn      git_stuff()
 #  @brief   clone or pull lfs-ci git repository in $HOME/lfs-ci
+#  @details If \$LFS_CI_ROOT already exists, git pull is invoked on
+#           branch development. Otherwise lfs-ci.git is cloned from
+#           git server. After cloning branch development is checked out.
 #  @return  <none>
 git_stuff() {
     echo "Git stuff..."
@@ -160,16 +180,16 @@ git_stuff() {
 #  @return  <none>
 jenkins_prepare_sandbox() {
     echo "Jenkins stuff..."
-    if [[ -d ${LOCAL_WORK_DIR}/${SANDBOX_USER} ]]; then 
-        echo "    Remove dir ${LOCAL_WORK_DIR}/${SANDBOX_USER}"
-        rm -rf ${LOCAL_WORK_DIR}/${SANDBOX_USER}
+    if [[ -d ${LOCAL_WORK_DIR}/${SANDBOX_USER}/${JENKINS_DIR} ]]; then 
+        echo "    Remove dir ${LOCAL_WORK_DIR}/${SANDBOX_USER}/${JENKINS_DIR}"
+        rm -rf ${LOCAL_WORK_DIR}/${SANDBOX_USER}/${JENKINS_DIR}
     fi
     echo "    Create dir ${JENKINS_HOME}/jobs"
     mkdir -p ${JENKINS_HOME}/jobs
 }
 
 ## @fn      jenkins_prepare_sandbox()
-#  @brief   copy over some jobs from LFS production jenkins.
+#  @brief   copy over jobs from production jenkins.
 #  @return  <none>
 jenkins_copy_jobs() {
     local excludes="--exclude=builds \
@@ -205,7 +225,7 @@ jenkins_copy_jobs() {
         fi
     done
 
-    if [[ ${LRC} == true ]]; then
+    if [[ "${LRC}" == "true" ]]; then
         echo "    Copy jobs from ${LRC_PROD_JENKINS_SERVER}"
         for JOB_PREFIX in LFS_CI_-_LRC_-_ LFS_Prod_-_LRC_-_ PKGPOOL_-_LRC_-_; do
             rsync -a ${excludes} ${LRC_PROD_JENKINS_SERVER}:${LRC_PROD_JENKINS_JOBS}/${JOB_PREFIX}* ${JENKINS_HOME}/jobs/
@@ -214,7 +234,7 @@ jenkins_copy_jobs() {
 }
 
 ## @fn      jenkins_disable_deploy_ci_scripting_job()
-#  @brief   disable some jobs in new sandbox Jenkins.
+#  @brief   disable job Admin_-_deployCiScripting in new sandbox Jenkins.
 #  @return  <none>
 jenkins_disable_deploy_ci_scripting_job() {
     [[ ! -f ${JENKINS_HOME}/jobs/Admin_-_deployCiScripting/config.xml ]] && return
@@ -223,7 +243,15 @@ jenkins_disable_deploy_ci_scripting_job() {
 }
 
 ## @fn      jenkins_copy_plugins()
-#  @brief   copy over plugins from LFS production jenkins.
+#  @brief   copy over plugins from production jenkins.
+#  @details If KEEP_JENKINS_PLUGINS == "true" and $1 == "copy", the plugins
+#           are copied from $JENKINS_HOME to /tmp. If KEEP_JENKINS_PLUGINS == "false"
+#           and $2 == "restore" the plugins are copied from /tmp to $JENKINS_HOME.
+#           If KEEP_JENKINS_PLUGINS == "false" the plugins are copied from production
+#           server. KEEP_JENKINS_PLUGINS == "true" can be used if coping from production
+#           server would take a long time because of slow network connection. This implies
+#           that sandbox already exists on the local machine of course and you want to
+#           set it up from scratch.
 #  @param   the mode (copy|restore).
 #  @return  <none>
 jenkins_plugins() {
@@ -232,33 +260,36 @@ jenkins_plugins() {
     [[ ! -d ${saveDir} ]] && mkdir -p ${saveDir}
 
     echo "Jenkins plugins"
-    if [[ ${KEEP_JENKINS_PLUGINS} == false && ${mode} == copy ]]; then
+    if [[ "${KEEP_JENKINS_PLUGINS}" == "false" && "${mode}" == "copy" ]]; then
         echo "    Copy plugins from ${PROD_JENKINS_SERVER}"
         rsync -a ${PROD_JENKINS_SERVER}:${PROD_JENKINS_HOME}/plugins ${saveDir}/
     fi
 
-    if [[ ${KEEP_JENKINS_PLUGINS} == true && ${mode} == "copy" ]]; then
-        echo "    Backup local plugins"
+    if [[ "${KEEP_JENKINS_PLUGINS}" == "true" && "${mode}" == "copy" ]]; then
+        echo "    Backup local plugins to ${saveDir}"
         cp -a ${JENKINS_PLUGINS} ${saveDir}
-    elif [[ ${mode} == "restore" ]]; then
-        echo "    Restore local plugins"
+    elif [[ "${mode}" == "restore" ]]; then
+        echo "    Restore local plugins from ${saveDir}"
         cp -a ${saveDir}/plugins ${JENKINS_HOME}
         rm -rf ${saveDir}
     fi
 }
 
 ## @fn      jenkins_get_configs()
-#  @brief   get configuration from LFS production jenkins.
+#  @brief   get configuration from production jenkins.
+#  @details Gets the config.xml files for the requested views
+#           from production server and put those files into
+#           /tmp/${VIEW}_config.xml.
 #  @return  <none>
 jenkins_get_configs() {
     echo "Get Jenkins view configuration from ${PROD_JENKINS_SERVER}..."
     for VIEW in $(echo ${BRANCH_VIEWS} | tr ',' ' '); do
-        if [[ ${VIEW} == trunk ]]; then
+        if [[ "${VIEW}" == "trunk" ]]; then
             echo "    Get ${VIEW} view configuration"
             curl -k https://${PROD_JENKINS_SERVER}/view/${VIEW}/config.xml --noproxy localhost > ${TMP}/${VIEW}_view_config.xml 2> /dev/null
         else
             local parentView=$(echo ${VIEW} | cut -c 3-)
-            [[ $(echo ${VIEW} | cut -c 1-2) == "MD" ]] && parentView=$(echo ${VIEW} | cut -c 4-)
+            [[ $(echo ${VIEW} | cut -c 1-2) == MD ]] && parentView=$(echo ${VIEW} | cut -c 4-)
             echo "    Get ${VIEW} view configuration"
             curl -k https://${PROD_JENKINS_SERVER}/view/${parentView}/view/${VIEW}/config.xml --noproxy localhost > ${TMP}/${VIEW}_view_config.xml 2> /dev/null
         fi
@@ -276,14 +307,15 @@ jenkins_get_configs() {
         curl -k https://${PROD_JENKINS_SERVER}/view/${parentTab}/view/${childTab}/config.xml --noproxy localhost > ${TMP}/${parentTab}_${childTab}_view_config.xml 2> /dev/null
     done
 
-    if [[ ${LRC} == true ]]; then
+    if [[ "${LRC}" == "true" ]]; then
         echo "    Get LRC view configuration"
         curl -k https://${LRC_PROD_JENKINS_SERVER}/view/LRC/config.xml --noproxy localhost > ${TMP}/LRC_view_config.xml 2> /dev/null
     fi
 }
 
 ## @fn      jenkins_start_sandbox()
-#  @brief   start new sandbox Jenkins in the background.
+#  @brief   start new sandbox Jenkins on local machine in the background
+#           via nohup and redirect stdout and stderr to $LOG_FILE.
 #  @return  <none>
 jenkins_start_sandbox() {
     echo "Export vars and start Jenkins server..."
@@ -298,13 +330,17 @@ jenkins_start_sandbox() {
     sleep 30
 }
 
-## @fn      jenkins_start_sandbox()
+## @fn      jenkins_configure_sandbox()
 #  @brief   configure new sandbox Jenkins.
+#  @details Take the views config.xml files from /tmp (see jenkins_get_configs()) and
+#           send them to local sandbox jenkins via the Jenkis HTTP API by using curl.
+#           Beforehand create the views (tabs) in sandbox by invoking setup-sandbox.gry
+#           via jenkins CLI.
 #  @return  <none>
 jenkins_configure_sandbox() {
     echo "Invoke groovy script on new sandbox..."
     java -jar ${JENKINS_CLI_JAR} -s http://localhost:${HTTP_PORT}/ groovy \
-        ${LFS_CI_ROOT}/sandbox/setup-sandbox.gry create_views ${ROOT_VIEWS} ${NESTED_VIEWS} ${BRANCH_VIEWS} ${LRC}
+        ${SANDBOX_SCRIPT_DIR}/setup-sandbox.gry create_views ${ROOT_VIEWS} ${NESTED_VIEWS} ${BRANCH_VIEWS} ${LRC}
 
     echo "Configure Jenkins top branch views (tabs) in new sandbox"
     for VIEW in $(echo ${BRANCH_VIEWS} | tr ',' ' '); do
@@ -326,14 +362,14 @@ jenkins_configure_sandbox() {
         curl http://localhost:${HTTP_PORT}/view/${parentTab}/view/${childTab}/config.xml --noproxy localhost --data-binary @${TMP}/${parentTab}_${childTab}_view_config.xml
     done
 
-    if [[ ${LRC} == true ]]; then
+    if [[ "${LRC}" == "true" ]]; then
         echo "    Configure LRC view"
         curl http://localhost:${HTTP_PORT}/view/LRC/config.xml --noproxy localhost --data-binary @${TMP}/LRC_view_config.xml
     fi
 }
 
 ## @fn      jenkins_configure_primary_view()
-#  @brief   set trunk view as the primary view.
+#  @brief   set trunk view as the primary view in sandbox.
 #  @return  <none>
 jenkins_configure_primary_view() {
     echo "Set primary view to trunk"
@@ -341,7 +377,7 @@ jenkins_configure_primary_view() {
 }
 
 ## @fn      jenkins_configure_master_executors()
-#  @brief   set number of executors for master.
+#  @brief   set number of executors for master to 4 in sandbox.
 #  @return  <none>
 jenkins_configure_master_executors() {
     local numExecutors=4
@@ -350,7 +386,7 @@ jenkins_configure_master_executors() {
 }
 
 ## @fn      jenkins_configure_version()
-#  @brief   set version number of Jenkins
+#  @brief   set version number of Jenkins in sandbox.
 #  @return  <none>
 jenkins_configure_version() {
     echo "Set version number of Jenkins to ${JENKNIS_VERSION}"
@@ -358,7 +394,7 @@ jenkins_configure_version() {
 }
 
 ## @fn      jenkins_configure_node_properties()
-#  @brief   set properties for jenkins master node
+#  @brief   set properties for jenkins master node in sandbox.
 #  @return  <none>
 jenkins_configure_node_properties() {
     sed -i -e 's,<label></label>,<label>master ulm  singleThreads testDummyHost multiThreads</label>,' ${JENKINS_HOME}/config.xml
@@ -382,10 +418,10 @@ jenkins_configure_node_properties() {
 }
 
 ## @fn      jenkins_disable_build_jobs()
-#  @brief   disable all jobs ending matching .*_Build$
+#  @brief   disable all jobs ending matching .*_Build$ in sandbox.
 #  @return  <none>
 jenkins_disable_build_jobs() {
-    if [[ ${DISABLE_BUILD_JOBS} == true ]]; then
+    if [[ "${DISABLE_BUILD_JOBS}" == "true" ]]; then
         echo "Disable all .*_Build$ jobs"
         find ${JENKINS_HOME}/jobs -type f -wholename "*_Build/config.xml" \
             -exec sed -i -e 's,<disabled>false</disabled>,<disabled>true</disabled>,' {} \;
@@ -393,7 +429,7 @@ jenkins_disable_build_jobs() {
 }
 
 ## @fn      jenkins_reload_config()
-#  @brief   reload jenkins config from disk.
+#  @brief   reload jenkins config from disk on sandbox.
 #  @return  <none>
 jenkins_reload_config() {
     echo "Reload Jenkins configuration from disk"
@@ -419,25 +455,42 @@ post_actions() {
 jenkins_stuff() {
     jenkins_plugins "copy"
     jenkins_prepare_sandbox
-    jenkins_copy_jobs
-    jenkins_disable_deploy_ci_scripting_job
-    jenkins_get_configs
+#    jenkins_copy_jobs
+#    jenkins_disable_deploy_ci_scripting_job
+#    jenkins_get_configs
     jenkins_plugins "restore"
-    jenkins_start_sandbox
-    jenkins_configure_sandbox
-    jenkins_configure_primary_view
-    jenkins_configure_master_executors
-    jenkins_configure_version
-    jenkins_configure_node_properties
-    jenkins_disable_build_jobs
-    jenkins_reload_config
+#    jenkins_start_sandbox
+#    jenkins_configure_sandbox
+#    jenkins_configure_primary_view
+#    jenkins_configure_master_executors
+#    jenkins_configure_version
+#    jenkins_configure_node_properties
+#    jenkins_disable_build_jobs
+#    jenkins_reload_config
+}
+
+remove_sandbox() {
+    local ans=""
+    read -p "Removing ${JENKINS_HOME} and $LFS_CI_ROOT} (y|N): " ans
+    if [[ "${ans}" == "y" || "${ans}" == "Y" ]]; then
+        echo rm -rf ${JENKINS_HOME}
+        echo rm -rf ${LFS_CI_ROOT}
+    else
+        echo "Nothing was deleted"
+    fi
 }
 
 main() {
-    if [[ $1 == --help || $1 == -h ]]; then
+    if [[ "$1" == "--help" || "$1" == "-h" ]]; then
         help
         exit 0
     fi
+
+    if [[ "${PURGE_SANDBOX}" == "true" ]]; then
+        remove_sandbox
+        exit $?
+    fi
+
     pre_actions
     git_stuff
     jenkins_stuff
