4e52955f99da8b6737e78d09c8be92ba6139f650
Bernhard Minks <bernhard.minks.ext@nokia.com>
2015-08-17 18:07:19 +0200

    disable pronto check

--- lib/customSCM.release.sh
+++ lib/customSCM.release.sh
@@ -36,10 +36,10 @@ actionCompare() {
     local changelog=$(createTempFile)
     _createChangelog ${oldUpstreamBuildNumber} ${upstreamBuildNumber} ${changelog}
 
-    if egrep -q -e '%FIN %PR=[0-9]+ESPE[09-]+' ${changelog} ; then
-        info "found in comments '%FIN %PR=<pronto>', trigger build"
-        exit 0
-    fi
+#    if egrep -q -e '%FIN %PR=[0-9]+ESPE[09-]+' ${changelog} ; then
+#        info "found in comments '%FIN %PR=<pronto>', trigger build"
+#        exit 0
+#    fi
 
     info "no pronto found between ${oldUpstreamProjectName}#${oldUpstreamBuildNumber} and ${upstreamProjectName}#${upstreamBuildNumber} => No build."
     exit 1
