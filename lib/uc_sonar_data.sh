3e00e535a20f795333c54ac1a6455d3953a40966
Meinolf Vogt <meinolf.vogt@nsn.com>
2015-09-18 12:45:06 +0200

    added copying of Sonar data for SCT

--- lib/uc_sonar_data.sh
+++ lib/uc_sonar_data.sh
@@ -5,11 +5,11 @@
 
 [[ -z ${LFS_CI_SOURCE_artifacts}       ]] && source ${LFS_CI_ROOT}/lib/artifacts.sh
 
-## @fn      usecase_LFS_COPY_SONAR_DATA()
-#  @brief   copy the files used for sonar to userContent directory
+## @fn      usecase_LFS_COPY_SONAR_UT_DATA()
+#  @brief   copy the unittest data used for sonar to Jenkins userContent directory
 #  @param   <none>
 #  @return  <none>
-usecase_LFS_COPY_SONAR_DATA() {
+usecase_LFS_COPY_SONAR_UT_DATA() {
 
     requiredParameters JOB_NAME
 
@@ -20,9 +20,57 @@ usecase_LFS_COPY_SONAR_DATA() {
 
     local workspace=$(getWorkspaceName)
     mustHaveWorkspaceName
-    
-    copyFileToUserContentDirectory ${workspace}/${sonarDataPath}/coverage.xml.gz sonar/${targetType}
-    copyFileToUserContentDirectory ${workspace}/${sonarDataPath}/testcases.merged.xml.gz sonar/${targetType}
+    local userContentPath=sonar/UT/${targetType}
 
+    _copy_Sonar_Data_to_userContent ${workspace}/${sonarDataPath} sonar/UT/${targetType}
+    
     return 0
 }
+
+## @fn      usecase_LFS_COPY_SONAR_SCT_DATA()
+#  @brief   copy the system component test data used for sonar to Jenkins userContent directory
+#  @param   <none>
+#  @return  <none>
+usecase_LFS_COPY_SONAR_SCT_DATA() {
+
+    requiredParameters JOB_NAME
+
+    local workspace=$(getWorkspaceName)
+    mustHaveWorkspaceName
+
+    for targetType in FSMr3 FSMr4
+    do
+
+        local sonarDataPath=$(eval echo $(getConfig LFS_CI_unittest_coverage_data_path))
+        local userContentPath=sonar/SCT/${targetType}
+
+        _copy_Sonar_Data_to_userContent ${workspace}/${sonarDataPath} ${userContentPath}
+
+    done
+    return 0
+}
+
+## @fn      _copy_Sonar_Data_to_userContent()
+#  @brief   copy the sonar data files coverage.xml.gz and testcases.merged.xml.gz 
+#  @param   {sonarDataDir}   directory where the files to be copied will be found
+#  @param   {userContentDir} directory (in Jenkins/userContent) where the files will be copied to
+#  @return  <none>
+_copy_Sonar_Data_to_userContent () {
+
+    local sonarDataDir=$1
+    local userContentDir=$2
+
+
+    for dataFile in coverage.xml.gz testcases.merged.xml.gz 
+    do
+        if [[ -e ${sonarDataDir}/${dataFile} ]] ; then
+            debug  now copying ${sonarDataDir}/${dataFile} to ${userContentDir} ...
+            copyFileToUserContentDirectory ${sonarDataDir}/${dataFile} ${userContentDir}
+        else
+            info  ${sonarDataDir}/${dataFile} not found!
+        fi
+    done
+    
+    return 0
+}
+
--- test/56_uc_copy_sonar_data.sh
+++ test/56_uc_copy_sonar_data.sh
@@ -14,7 +14,7 @@ oneTimeSetUp() {
         case ${1} in
             jenkinsMasterServerHostName)        echo localhost ;;
             jenkinsMasterServerPath)            echo ${JENKINS_ROOT}/home ;;
-            LFS_CI_unittest_coverage_data_path) echo bld/bld-unittests-${target}_fsmddal/results/__artifacts ;;
+            LFS_CI_unittest_coverage_data_path) echo ${DATA_PATH} ;;
             *)                                  echo $1
         esac
 
@@ -37,11 +37,23 @@ oneTimeTearDown() {
 }
 
 setUp() {
-    export WORKSPACE=$(createTempDirectory)
-    export JENKINS_ROOT=$(createTempDirectory)
+    # export WORKSPACE=$(createTempDirectory)
+    # export JENKINS_ROOT=$(createTempDirectory)
+    export WORKSPACE=/tmp/vm048635/workspace
+    export JENKINS_ROOT=/tmp/vm048635/jenkinsroot
 
-    for target in fsmr3 fsmr4 
+    for TARGET in fsmr3 fsmr4 
     do
+        export DATA_PATH=bld/bld-unittests-${TARGET}_fsmddal/results/__artifact
+        export SONAR_DATA_PATH=$(getConfig LFS_CI_unittest_coverage_data_path)
+        mkdir -p ${WORKSPACE}/${SONAR_DATA_PATH}
+        touch ${WORKSPACE}/${SONAR_DATA_PATH}/coverage.xml.gz
+        touch ${WORKSPACE}/${SONAR_DATA_PATH}/testcases.merged.xml.gz
+    done
+
+    for TARGET in FSMr3 FSMr4 
+    do
+        export DATA_PATH=src-test/src/unittest/testsuites/continousintegration/coverage/summary/__html/LCOV_${TARGET}
         export SONAR_DATA_PATH=$(getConfig LFS_CI_unittest_coverage_data_path)
         mkdir -p ${WORKSPACE}/${SONAR_DATA_PATH}
         touch ${WORKSPACE}/${SONAR_DATA_PATH}/coverage.xml.gz
@@ -52,35 +64,50 @@ setUp() {
 }
 
 tearDown() {
-    rm -rf ${WORKSPACE}
+    # rm -rf ${WORKSPACE}
     return 
 }
 
-test_FSMr3() {
+test_UT_FSMr3() {
     export JOB_NAME=LFS_CI_-_trunk_-_Build_-_FSM-r3-UT_-_fsmr3_fsmddal
-    export target=fsmr3
-    assertTrue "usecase_LFS_COPY_SONAR_DATA for FSMr3 failed!" "usecase_LFS_COPY_SONAR_DATA"
+    export DATA_PATH=bld/bld-unittests-fsmr3_fsmddal/results/__artifact
+    assertTrue "usecase_LFS_COPY_SONAR_UT_DATA for FSMr3 failed!" "usecase_LFS_COPY_SONAR_UT_DATA"
     
-    check_FilesExist
+    local targetType=$(getSubTaskNameFromJobName)
+    local sonarPath=sonar/UT/${targetType}
+    check_FilesExist ${sonarPath}
 
     return
 }
 
-test_FSMr4() {
+test_UT_FSMr4() {
     export JOB_NAME=LFS_CI_-_trunk_-_Build_-_FSM-r4-UT_-_fsmr4_fsmddal
-    export target=fsmr4
-    assertTrue "usecase_LFS_COPY_SONAR_DATA for FSMr4 failed!" "usecase_LFS_COPY_SONAR_DATA"
+    export DATA_PATH=bld/bld-unittests-fsmr4_fsmddal/results/__artifact
+    assertTrue "usecase_LFS_COPY_SONAR_UT_DATA for FSMr4 failed!" "usecase_LFS_COPY_SONAR_UT_DATA"
 
-    check_FilesExist
+    local targetType=$(getSubTaskNameFromJobName)
+    local sonarPath=sonar/UT/${targetType}
+    check_FilesExist ${sonarPath}
+
+    return
+}
+
+test_SCT() {
+    export DATA_PATH='src-test/src/unittest/testsuites/continousintegration/coverage/summary/__html/LCOV_${targetType}'
+    assertTrue "usecase_LFS_COPY_SONAR_SCT_DATA failed!" "usecase_LFS_COPY_SONAR_SCT_DATA"
+    
+    local sonarPath=sonar/SCT/FSMr3
+    check_FilesExist ${sonarPath}
+    local sonarPath=sonar/SCT/FSMr4
+    check_FilesExist ${sonarPath}
 
     return
 }
 
 check_FilesExist() {
     # check that files exist
-    local targetType=$(getSubTaskNameFromJobName)
-    assertTrue "coverage.xml.gz not found!"         "[[ -e ${JENKINS_ROOT}/home/userContent/sonar/${targetType}/coverage.xml.gz ]]"
-    assertTrue "testcases.merged.xml.gz not found!" "[[ -e ${JENKINS_ROOT}/home/userContent/sonar/${targetType}/testcases.merged.xml.gz ]]"
+    assertTrue "coverage.xml.gz not found!"         "[[ -e ${JENKINS_ROOT}/home/userContent/${1}/coverage.xml.gz ]]"
+    assertTrue "testcases.merged.xml.gz not found!" "[[ -e ${JENKINS_ROOT}/home/userContent/${1}/testcases.merged.xml.gz ]]"
 
     return 0
 }
