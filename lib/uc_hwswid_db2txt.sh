5bed75618f7af405112604e6210e876b59c23aed
Reiner Biefel <reiner.biefel@nsn.com>
2015-09-25 12:08:21 +0200

    added new uc_admin_hwswid_db2txt

--- lib/uc_hwswid_db2txt.sh
+++ lib/uc_hwswid_db2txt.sh
@@ -9,21 +9,18 @@
 #  @param   <none>
 #  @return  <none>
 usecase_HWSWID_DB2TXT() {
-    requiredParameters JOB_NAME
-
     export workspace=$(getWorkspaceName)
     mustHaveWorkspaceName
     mustHaveCleanWorkspace
 
     export hwswidWorkdirDb=${workspace}/HwSwId.DB
-    mustHaveValue "${hwswidWorkdirDb}" "hwswidWorkdirDb"
     mkdir -p ${hwswidWorkdirDb}
     cd ${hwswidWorkdirDb}
 
     createHwSwIdTxtFile
-    HwSwIdToSubVersion
+    hwSwIdToSubVersion
 
-    return
+    return 0
 }
 
 ## @fn      createHwSwIdTxtFile
@@ -41,43 +38,43 @@ createHwSwIdTxtFile() {
     do
         for firmewaretype in $(getConfig LFS_CI_HWSWID_FIRMWARETYPE -t hw_platform:${hw_platform})
         do
-            datafrommysql HwSwId_${firmewaretype}.txt "firmwaretype=\"${firmewaretype}\""  "$(getConfig LFS_CI_HWSWID_DB_TABLE -t hw_platform:${hw_platform})"
+            hwswidDataFromMysql HwSwId_${firmewaretype}.txt "firmwaretype=\"${firmewaretype}\""  "$(getConfig LFS_CI_HWSWID_DB_TABLE -t hw_platform:${hw_platform})"
         done
     done
 
     # FSM-r3 for basebandfpga
     for basebandfpga in $(getConfig LFS_CI_HWSWID_BASEBANDFPGA -t hw_platform:fsmr3)
     do
-        datafrommysql HwSwId_${basebandfpga}.txt "basebandfpga=\"${basebandfpga}\""  "$mysqlTableFsmr3"
+        hwswidDataFromMysql HwSwId_${basebandfpga}.txt "basebandfpga=\"${basebandfpga}\""  "$mysqlTableFsmr3"
     done
 
-    datafrommysql HwSwId_UBOOT.txt  'boardname != "FSPN" and boardname != "FIFC"'  "$mysqlTableFsmr3"
-    removeminor HwSwId_UBOOT.txt
+    hwswidDataFromMysql HwSwId_UBOOT.txt  'boardname != "FSPN" and boardname != "FIFC"'  "$mysqlTableFsmr3"
+    hwswidRemoveMinorVersion HwSwId_UBOOT.txt
     rawDebug "HwSwId_UBOOT.txt"
 
-    datafrommysql HwSwId_UBOOT_FSMR4.txt  '1=1'  "$mysqlTableFsmr4"
-    removeminor HwSwId_UBOOT_FSMR4.txt
+    hwswidDataFromMysql HwSwId_UBOOT_FSMR4.txt  '1=1'  "$mysqlTableFsmr4"
+    hwswidRemoveMinorVersion HwSwId_UBOOT_FSMR4.txt
     rawDebug "HwSwId_UBOOT_FSMR4.txt"
 
-    datafrommysql HwSwId_UBOOT_FSMR4_FCT.txt  'boardname = "FCTJ" or boardname = "FSCA"'  "$mysqlTableFsmr4"
-    removeminor HwSwId_UBOOT_FSMR4_FCT.txt
+    hwswidDataFromMysql HwSwId_UBOOT_FSMR4_FCT.txt  'boardname = "FCTJ" or boardname = "FSCA"'  "$mysqlTableFsmr4"
+    hwswidRemoveMinorVersion HwSwId_UBOOT_FSMR4_FCT.txt
     rawDebug "HwSwId_UBOOT_FSMR4_FCT.txt"
-    datafrommysql HwSwId_UBOOT_FSMR4_FSP.txt  'boardname like "FSP%"'  "$mysqlTableFsmr4"
-    removeminor HwSwId_UBOOT_FSMR4_FSP.txt
+    hwswidDataFromMysql HwSwId_UBOOT_FSMR4_FSP.txt  'boardname like "FSP%"'  "$mysqlTableFsmr4"
+    hwswidRemoveMinorVersion HwSwId_UBOOT_FSMR4_FSP.txt
     rawDebug "HwSwId_UBOOT_FSMR4_FSP.txt"
 
-    datafrommysql HwSwId_FSPN.txt  'boardname = "FSPN"'  "$mysqlTableFsmr3"
-    removeminor HwSwId_FSPN.txt
+    hwswidDataFromMysql HwSwId_FSPN.txt  'boardname = "FSPN"'  "$mysqlTableFsmr3"
+    hwswidRemoveMinorVersion HwSwId_FSPN.txt
     rawDebug "HwSwId_FSPN.txt"
 }
 
-## @fn      createHwSwIdTxtFile
+## @fn      hwswidDataFromMysql
 #  @brief   execute sql command to create intermediate HwSwId_<firmware>.txt files out of the database
 #  @param   HwSwId_xxx.txt file
 #  @param   sql where clause
 #  @param   mysql table name
 #  @return  <none>
-datafrommysql() {
+hwswidDataFromMysql() {
     local hwswidTxtFile="$1"
     mustHaveValue "${hwswidTxtFile}" "hwswidTxtFile"
     local query="$2"
@@ -94,80 +91,44 @@ datafrommysql() {
     ### dems18x0: 2015-09-22: execute before mysql command is not working !
     ${mysql_cli} -B -r -s -e 'SELECT DISTINCT `hw_sw_id` FROM '"$currentMysqlTable"' WHERE '"$query" >"$hwswidTxtFile".tmp
 
-    cat "$hwswidTxtFile".tmp | grep -v '^$' | grep -v 'NULL' | sort -u >"$hwswidTxtFile"
+    grep -v -e '^$' -e 'NULL' "$hwswidTxtFile".tmp | sort -u >"$hwswidTxtFile"
     rm "$hwswidTxtFile.tmp"
 
-    cat $hwswidTxtFile
+    rawDebug $hwswidTxtFile
 }
 
 
-## @fn      removeminor
+## @fn      hwswidRemoveMinorVersion
 #  @brief   substitute all latest 2 digits of HwSwId entries 01
 #  @param   HwSwId_xxx.txt file
 #  @return  <none>
-removeminor() {
+hwswidRemoveMinorVersion() {
     local hwswidTxtFile="$1"
     mustHaveValue "${hwswidTxtFile}" "hwswidTxtFile"
 
     sed -e 's/..$/01/' "${hwswidTxtFile}" | sort -u  >"${hwswidTxtFile}".new
     mv "${hwswidTxtFile}".new "${hwswidTxtFile}"
+
+    return 0
 }
 
 
-## @fn      HwSwIdToSubVersion
+## @fn      hwSwIdToSubVersion
 #  @brief   merge created HwSwId.txt from DB with SVN file and do upload to SVN if chnaged
 #  @param   <none>
 #  @return  <none>
-HwSwIdToSubVersion() {
+hwSwIdToSubVersion() {
     local hwswidWorkdirSvn=${workspace}/HwSwId.SVN
-    mustHaveValue "${hwswidWorkdirSvn}" "hwswidWorkdirSvn"
 
     for url in $(getConfig LFS_CI_HWSWID_URLS)
     do
-        info Writing HwSwId to URL ${url} ...
+        info Updating HwSwId when needed to URL ${url} ...
 
-        # Check if there is a HwSwId_* file at this URL
-        local hwswidTxtFound=false
-        local tmpfile=$(mktemp /tmp/HwSwId1.XXXXXXXX)
-        svn list "${url}" >"${tmpfile}"
-
-        while read filename
-        do
-            case "${filename}"
-            in HwSwId_*.txt)    hwswidTxtFound=true
-            esac
-        done <"${tmpfile}"
-        rm -f "${tmpfile}"
-        ${hwswidTxtFound} || error "${url}: no HwSwId...txt file found"
         rm -rf "${hwswidWorkdirSvn}"
-        svn co "${url}" "${hwswidWorkdirSvn}"
-
-        # Diff generated HwSwId file from DB with downloaded HwSwId file from SVN
-        local diffFound=false
-        for FILE in "${hwswidWorkdirSvn}"/HwSwId*.txt
-        do
-            filename=$(basename "$FILE")
-            mustHaveValue "${filename}" "filename"
-            [ -f "${hwswidWorkdirDb}"/"${filename}" ] || error "${filename}" not in current workdir "${hwswidWorkdirDb}"
-            diff "${hwswidWorkdirDb}"/"${filename}" "$FILE" || {
-                diffFound=true
-                cp -f "${hwswidWorkdirDb}"/"${filename}" "$FILE"
-            }
-        done
-
-        # if there is a changed hwswid file for that branch then commit it
-        if $diffFound
-        then
-            cd "${hwswidWorkdirSvn}"
-            svn info | grep "^URL:"
-            svn diff
-            info TODO ENABLE: svn commit -m "BTSPS-1657 IN psulm: update HwSwId NOJCHK"
-            cd -
-        else
-            info No HwSwId changes in ${url}
-        fi
+        svnCheckout ${url} "${hwswidWorkdirSvn}"
+        cp -a ${hwswidWorkdirDb}/* "${hwswidWorkdirSvn}"/
+        svn diff ${hwswidWorkdirSvn}
+        info TODO ENABLE: svn commit -m "update HwSwId NOJCHK" ${hwswidWorkdirSvn}
 
     done
-
-    rm -rf "${hwswidWorkdirSvn}"
 }
--- test/57_uc_hwswid_db2txt.sh
+++ test/57_uc_hwswid_db2txt.sh
@@ -16,8 +16,8 @@ oneTimeSetUp() {
     createHwSwIdTxtFile() {
         mockedCommand "createHwSwIdTxtFile $@"
     }
-    HwSwIdToSubVersion() {
-        mockedCommand "HwSwIdToSubVersion $@"
+    hwSwIdToSubVersion() {
+        mockedCommand "hwSwIdToSubVersion $@"
     }
 
     # create a temp file.cfg
@@ -25,20 +25,20 @@ oneTimeSetUp() {
 
     echo "LFS_CI_HWSWID_WORKDIR=/home/dems18x0/tmp/HwSwId" > ${UT_CFG_FILE}
 
-    echo "MYSQL_db_name     < databaseName:hwswid_database > = flexibtshw               " >> ${UT_CFG_FILE}
-    echo "MYSQL_db_username < databaseName:hwswid_database > = flexibtshw_ro            " >> ${UT_CFG_FILE}
-    echo "MYSQL_db_password < databaseName:hwswid_database > =                          " >> ${UT_CFG_FILE}
-    echo "MYSQL_db_hostname < databaseName:hwswid_database > = psweb.nsn-net.net        " >> ${UT_CFG_FILE}
+    echo "MYSQL_db_name     < databaseName:hwswid_database > = test_flexibtshw               " >> ${UT_CFG_FILE}
+    echo "MYSQL_db_username < databaseName:hwswid_database > = test_flexibtshw_ro            " >> ${UT_CFG_FILE}
+    echo "MYSQL_db_password < databaseName:hwswid_database > =                               " >> ${UT_CFG_FILE}
+    echo "MYSQL_db_hostname < databaseName:hwswid_database > = test_psweb.nsn-net.net        " >> ${UT_CFG_FILE}
 
-    echo "LFS_CI_HWSWID_DB_TABLE < hw_platform:fsmr3 > = flexibtshw.v_fsmr3boards       " >> ${UT_CFG_FILE}
-    echo "LFS_CI_HWSWID_DB_TABLE < hw_platform:fsmr4 > = flexibtshw.v_fsmr4boards       " >> ${UT_CFG_FILE}
+    echo "LFS_CI_HWSWID_DB_TABLE < hw_platform:fsmr3 > = test_flexibtshw.v_fsmr3boards       " >> ${UT_CFG_FILE}
+    echo "LFS_CI_HWSWID_DB_TABLE < hw_platform:fsmr4 > = test_flexibtshw.v_fsmr4boards       " >> ${UT_CFG_FILE}
 
     echo "LFS_CI_HWSWID_FIRMWARETYPE < hw_platform:fsmr3 > = uCCSSA CCSSAH uBMU CCSSA EC2 EC2G BMU0 BMU1 BMU2 BMU4 BMUM BMUK BMUP   " >> ${UT_CFG_FILE}
     echo "LFS_CI_HWSWID_FIRMWARETYPE < hw_platform:fsmr4 > = BMUQ BMUQ_X11 PCO2PEX PICASSOX02 PICASSOX03    " >> ${UT_CFG_FILE}
 
     echo "LFS_CI_HWSWID_BASEBANDFPGA < hw_platform:fsmr3 > = CPRIMON CPRIIF D4P " >> ${UT_CFG_FILE}
 
-    echo "LFS_CI_HWSWID_URLS = ${BTS_SC_LFS_url}/os/trunk/fsmr3/src-fsmpsl/src/trailer.d ${BTS_SC_LFS_url}/os/trunk/fsmr3/src-fsmfirmware/src/trailer.d ${BTS_SC_LFS_url}/os/trunk/fsmr3/src-fsmbrm/src/trailer.d   " >> ${UT_CFG_FILE}
+    echo "LFS_CI_HWSWID_URLS = XXX/os/trunk/fsmr3/src-fsmpsl/src/trailer.d XXX/os/trunk/fsmr3/src-fsmfirmware/src/trailer.d XXX/os/trunk/fsmr3/src-fsmbrm/src/trailer.d   " >> ${UT_CFG_FILE}
 
     export LFS_CI_CONFIG_FILE=${UT_CFG_FILE}
 
@@ -78,8 +78,6 @@ test_setup() {
 }
 
 test_usecase_HWSWID_DB2TXT() {
-    JOB_NAME=Admin_-_HwSwId_db2txt
-
     assertTrue "usecase_HWSWID_DB2TXT"
 
     return
@@ -91,27 +89,27 @@ test_createHwSwIdTxtFile() {
     return
 }
 
-test_datafrommysql_HwSwId_UBOOT_FSMR4_TXT() {
+test_hwswidDataFromMysql_HwSwId_UBOOT_FSMR4_TXT() {
     hwswidTxtFile=$(createTempFile)
     query='1=1'
-    currentMysqlTable="flexibtshw.v_fsmr4boards"
-    assertTrue "datafrommysql ${hwswidTxtFile} ${query} ${currentMysqlTable}"
+    currentMysqlTable="test_table"
+    assertTrue "hwswidDataFromMysql ${hwswidTxtFile} ${query} ${currentMysqlTable}"
 
     cat ${hwswidTxtFile}
 
     return
 }
 
-test_removeminor() {
-    assertTrue "removeminor ${HWSWID_TXT_FILE}"
+test_hwswidRemoveMinorVersion() {
+    assertTrue "hwswidRemoveMinorVersion ${HWSWID_TXT_FILE}"
 
-    cat ${HWSWID_TXT_FILE}
+    #cat ${HWSWID_TXT_FILE}
 
     return
 }
 
-test_HwSwIdToSubVersion() {
-    assertTrue "HwSwIdToSubVersion"
+test_hwSwIdToSubVersion() {
+    assertTrue "hwSwIdToSubVersion"
 
     return
 }
