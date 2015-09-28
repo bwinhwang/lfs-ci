d896add55cf21b2fa465cf799e37dfe45f1b1ee6
Bernhard Minks <bernhard.minks.ext@nokia.com>
2015-09-28 12:39:04 +0200

    fix for release usecase source tag

--- lib/uc_release_create_source_tag.sh
+++ lib/uc_release_create_source_tag.sh
@@ -136,8 +136,9 @@ _createUsedRevisionsFile() {
         local cfg=$(cut -d- -f3 <<< ${dirName})
         local tagDirectory=$(getConfig LFS_PROD_uc_release_source_tag_directory -t cfg:${cfg})
         info "using tag dir ${tagDirectory} for cfg ${cfg}"
+        execute touch ${workspace}/rev/${tagDirectory}
 
-        execute -l ${workspace}/rev/${tagDirectory} sort -u ${revisionFile} 
+        execute -l ${workspace}/rev/${tagDirectory} sort -u ${revisionFile} ${workspace}/rev/${tagDirectory}
         rawDebug ${workspace}/rev/${tagDirectory}
     done
 
