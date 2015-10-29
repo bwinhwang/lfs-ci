93f7c8392857cb1120b5eee118b64d6fb49fcdab
Erwin Ambrosch <erwin.ambrosch.ext@nokia.com>
2015-10-29 12:06:30 +0100

    Get DB name from config.

--- lib/uc_branching.sh
+++ lib/uc_branching.sh
@@ -7,7 +7,7 @@
 source ${LFS_CI_ROOT}/lib/common.sh
 source ${LFS_CI_ROOT}/lib/database.sh
 
-## @fn      createBranch()
+## @fn      usecase_LFS_BRANCHING_CREATE_BRANCH()
 #  @brief   create a LFS branch
 #  @param   <none>
 #  @retrun  <none>
@@ -15,7 +15,7 @@ usecase_LFS_BRANCHING_CREATE_BRANCH() {
     ${LFS_CI_ROOT}/bin/createBranch.sh
 }
 
-## @fn      deleteBranch()
+## @fn      usecase_LFS_BRANCHING_DELETE_BRANC()
 #  @brief   delete a LFS branch
 #  @param   <none>
 #  @retrun  <none>
@@ -23,20 +23,23 @@ usecase_LFS_BRANCHING_DELETE_BRANCH() {
     ${LFS_CI_ROOT}/bin/deleteBranch.sh
 }
 
+## @fn      usecase_LFS_BRANCHING_MD_FOR_LRC()
+#  @brief   add MD branch for LRC
+#  @param   <none>
+#  @retrun  <none>
 usecase_LFS_BRANCHING_MD_FOR_LRC() {
     mustHaveDatabaseCredentials
-    mustHaveValue ${BRANCH} "BRANCH is missing"
-    mustHaveValue ${PS_BRANCH} "PS_BRANCH is missing"
-    mustHaveValue ${ACTION} "ACTION is missing"
+    requiredParameters BRANCH PS_BRANCH ACTION
+    local dbName=$(getConfig MYSQL_db_name)
     [[ ! ${BRANCH} =~ ^LRC_ ]] && BRANCH=LRC_${BRANCH}
     case ${ACTION} in
         create)
             info "Insert new branch into tables branches and ps_branches."
-            echo "CALL new_ps_branch_for_md_lrc('"${BRANCH}"', '"${PS_BRANCH}"', '"${ECL_URL}"')" | ${mysql_cli} -Dlfspt_lrc
+            echo "CALL new_ps_branch_for_md_lrc('"${BRANCH}"', '"${PS_BRANCH}"', '"${ECL_URL}"')" | ${mysql_cli} -D${dbName}
         ;;
         close)
             info "Close branch in tables branches and ps_branches."
-            echo "CALL close_ps_branch_for_md_lrc('"${PS_BRANCH}"')" | ${mysql_cli} -Dlfspt_lrc
+            echo "CALL close_ps_branch_for_md_lrc('"${PS_BRANCH}"')" | ${mysql_cli} -D${dbName}
         ;;
     esac
 }
