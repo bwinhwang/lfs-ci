fb7df31c83db3a7a5eae5440190be30dfdbf8b7f
Bernhard Minks <bernhard.minks.ext@nokia.com>
2015-12-10 11:01:27 +0100

    fix for PSLSFPT-254

--- test/66_uc_release_send_release_note_createLfsRelReleaseNoteXml.sh
+++ test/66_uc_release_send_release_note_createLfsRelReleaseNoteXml.sh
@@ -55,7 +55,7 @@ test1() {
 execute mkdir -p ${WORKSPACE}/workspace/rel/bld/bld-externalComponents-summary
 execute cd ${WORKSPACE}/workspace/rel/
 execute grep sdk ${WORKSPACE}/workspace/bld/bld-externalComponents-summary/externalComponents
-execute -n ${LFS_CI_ROOT}/bin/getReleaseNoteXML -t PS_LFS_REL_BUILD_NAME -o PS_LFS_REL_OLD_BUILD_NAME -T OS -P LFS -f ${LFS_CI_ROOT}/etc/lfs-ci.cfg -L pronb-developer
+execute -n ${LFS_CI_ROOT}/bin/getReleaseNoteXML -t PS_LFS_REL_BUILD_NAME -o PS_LFS_REL_OLD_BUILD_NAME -T REL -P LFS -f ${LFS_CI_ROOT}/etc/lfs-ci.cfg -L pronb-developer
 EOF
     assertExecutedCommands ${expect}
 
