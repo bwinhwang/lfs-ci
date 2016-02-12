a9bf0542e10b371f1fd4fbf05886a39864f1cf1f
Bernhard Minks <bernhard.minks.ext@nokia.com>
2016-02-12 10:57:20 +0100

    update unittest.sh, run in parallel

--- bin/unitTest.sh
+++ bin/unitTest.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-set -o errexit
-
 export LFS_CI_ROOT=$PWD
 export USER=psulm
 export HOME=/path/to/home
 
-source ${LFS_CI_ROOT}/lib/logging.sh
-
-for file in test/*.sh ; do
-    info "executing unit test ${file}"
-    ${file} 
-done
-
+make clean
+make test -j 20
 exit 0
 
